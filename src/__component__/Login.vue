a1336992b1bfb5375d934d9001b9451861c1c5ad
HuaiYang <hycool.happy@163.com>
2019-02-28 16:19:27 +0800

    增加登录逻辑

--- src/__component__/Login.vue
+++ src/__component__/Login.vue
@@ -1,10 +1,46 @@
 <template>
-  <h1>this is login page</h1>
+  <div>
+    <input
+      type="text"
+      value="root"
+    >
+    <br>
+    <input
+      type="password"
+      value="123"
+    >
+    <br>
+    <input
+      type="button"
+      value="点击登录"
+      @click="login"
+    >
+  </div>
 </template>
 
 <script>
+  import network, { urlSearchParams } from '../__utils__/network';
+  
   export default {
-    name: 'Login'
+    name: 'Login',
+    methods: {
+      login() {
+        network.post('/p/c/getCaptcha').then((res) => {
+          console.log(res);
+          network.post('/p/c/login', urlSearchParams({
+            username: 'root',
+            password: 123,
+            captcha: res.data.captcha,
+            rememberMe: false,
+            lang: 'zh_CN',
+          })).then((r) => {
+            if (r.status === 200 && r.data.code === 0) {
+              window.location.href = window.location.origin;
+            }
+          });
+        });
+      }
+    }
   };
 </script>
 
--- src/__utils__/network.js
+++ src/__utils__/network.js
@@ -1,3 +1,19 @@
 import axios from 'axios';
+import router from '../__config__/router.config';
+
+axios.interceptors.response.use(response => response, (error) => {
+  if (error.response.status === 403) {
+    router.push('/login');
+  }
+  Promise.reject(error);
+});
+
+export const urlSearchParams = (data) => {
+  const params = new URLSearchParams();
+  Object.keys(data).forEach((key) => {
+    params.append(key, data[key]);
+  });
+  return params;
+};
 
 export default axios;
