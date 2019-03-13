5f3723b6a3ce549d34efc7b133e7d9fb6892e530
HuaiYang <hycool.happy@163.com>
2019-03-13 13:29:28 +0800

    修改标准界面动态模块管理逻辑

--- src/__component__/StandardTableList.vue
+++ src/__component__/StandardTableList.vue
@@ -15,38 +15,17 @@
 </template>
 
 <script>
-  import { mapActions, mapState } from 'vuex';
-  import router from '../__config__/router.config';
+  import { mapActions } from 'vuex';
   import AgTable from './StandardTable';
   import FormItemComponent from './FormItemComponent';
   import itemComponent from './itemComponent';
-  import { STANDARD_TABLE_COMPONENT_PREFIX } from '../constants/global';
-
-  const getComponentName = () => {
-    const { tableName, tableId } = router.currentRoute.params;
-    return `${STANDARD_TABLE_COMPONENT_PREFIX}.${tableName}.${tableId}`;
-  };
 
   export default {
     components: {
       AgTable, FormItemComponent
     },
-    computed: {
-      ...mapState(getComponentName(), {
-        ag: ({ ag }) => ag,
-        pageAttribute: ({ ag }) => ({
-          current: (ag.datas.start + ag.datas.defaultrange) / ag.datas.defaultrange,
-          total: ag.datas.totalRowCount,
-          'page-size-opts': ag.datas.selectrange,
-          'show-elevator': true,
-          'show-sizer': true,
-          'show-total': true
-        })
-      }),
-    },
     data() {
       return {
-        moduleStateKey: getComponentName(),
         searchData: {
           table: this.$route.params.tableName,
           startIndex: 0,
@@ -169,7 +148,6 @@
     },
     methods: {
       ...mapActions('global', ['updateAccessHistory']),
-      ...mapActions(getComponentName(), ['getQueryListForAg']),
       getQueryList() {
         const { agTableElement } = this.$refs;
         agTableElement.showAgLoading();
