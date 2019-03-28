8178c1a5ec2c4a9770dc1f95ba33bd22812b1fd8
HuaiYang <hycool.happy@163.com>
2019-03-28 10:06:51 +0800

    处理左右结构单对象横向布局基底框子

---
+++ src/__config__/store/horizontalTableDetail.js
@@ -0,0 +1,10 @@
+import actions from '../actions/horizontalTableDetail.actions';
+import mutations from '../mutations/horizontalTableDetail.mutations';
+
+export default () => ({
+  namespaced: true,
+  state: {
+  },
+  actions,
+  mutations
+});
