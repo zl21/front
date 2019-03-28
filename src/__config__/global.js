c18d8c489c21e63931b9f2be1ab5777611333c0f
xia.hj <xia.hj@burgeon.cn>
2019-03-28 09:14:54 +0800

    增加按钮操作新增逻辑

---
+++ src/__config__/global.js
@@ -0,0 +1,26 @@
+export const enableGateWay = true;
+
+// 需要走全局公共网关的接口
+export const globalGateWay = [
+  '/p/cs/getSubSystems',
+  '/p/cs/hello',
+  '/p/cs/getHistoryAndFavorite',
+  '/p/cs/getParamList',
+  '/p/cs/setUserParam',
+  '/p/c/login',
+  '/p/cs/logout',
+  '/p/cs/removeFromFavorite',
+  '/p/cs/addToFavorite',
+  '/p/cs/getUserConfig',
+  '/p/cs/setColPosition',
+  '/p/cs/recHistory',
+  '/p/cs/setHideColumn',
+  '/p/c/getCaptcha',
+  '/p/cs/getTableQuery',
+  '/p/cs/QueryList'
+];
+
+// 不走网关的接口
+export const ignoreGateWay = [
+  '/p/c/get_service_id',
+];
