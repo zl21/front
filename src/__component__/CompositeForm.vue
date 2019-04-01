ee163acafa7543195d559395ba6106cf46e032df
liu.li <li.liu@burgeon.cn>
2019-04-01 09:46:30 +0800

    单对象form 组件

---
+++ src/__component__/CompositeForm.vue
@@ -0,0 +1,27 @@
+<template>
+    展示form
+</template>
+
+<script>
+  export default {
+    name: "CompositeForm",
+    props: {
+      defaultData: {
+        type: Array,
+        default() {
+          return [];
+        }
+      },
+      type: {
+        type: String,
+        default() {
+          return '';
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
