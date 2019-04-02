bd83686e7ed207795938143ed1b81e3e548b9e66
liu.li <li.liu@burgeon.cn>
2019-04-02 17:03:49 +0800

    form z组件渲染

--- src/__component__/CompositeForm.vue
+++ src/__component__/CompositeForm.vue
@@ -3,14 +3,15 @@
     <div>
         <template  v-if="type === 'PanelForm'">
             <Collapse
-                    v-for="(item,index) in computdefaultData.addcolums" active-key="index"
-                    v-model="expand"
+                    v-for="(item,index) in computdefaultData" active-key="index"
+                    v-model="item.hrdisplay"
+                    @on-change="CollapseClose(index,item.hrdisplay)"
                 >
                 <Panel :key="index" title-type="center"  :name="item.hrdisplay" >
                     {{item.parentdesc}}
                     <div slot="content">
                         <FormItemComponent
-                          :form-item-lists="FormLists(item.childs)"
+                          :form-item-lists = "item.childs"
                           :defaultColumn="computdefaultData.objviewcol"
                         >
                         </FormItemComponent>
@@ -20,7 +21,7 @@
         </template>
         <template  v-if="type === ''">
             <FormItemComponent  >
-                :form-item-lists="FormLists(computdefaultData)"
+                :form-item-lists="FormLists(computdefaultData.inpubobj)"
             </FormItemComponent>
         </template>
     </div>
@@ -52,117 +53,62 @@
     },
     data() {
       return {
-        newdefaultData:[],
+        newdefaultData:{},
         expand:'expand'
       };
     },
     computed: {
       computdefaultData(){
-          console.log(this.defaultData);
-          return this.defaultData;
-      },
-      FormLists(){
-        return function (arr) {
-          let items = [];
-          items = JSON.parse(JSON.stringify(arr)).reduce((array, current, itemIndex) => {
-            let obj = {} ;
-                  obj.row = current.row ? current.row : 1;
-                  obj.col = current.col ? current.col : 1;
-                  obj.component = ItemComponent;
-                  obj.item = {
-                    type: this.checkDisplay(current),
-                    title: current.name,
-                    field: current.colname,
-                    value: this.defaultValue(current),
-                    inputname: current.inputname,
-                    props: {},
-                    event: {},
-                    validate: {
-                    }
-                  };
-
-            // 带有combobox的添加到options属性中
-            //obj.item.props.type = this.checkDisplay(current);
-
-            if (current.combobox) {
-              const arr = current.combobox.reduce((sum, item) => {
-                sum.push({
-                  label: item.limitdesc,
-                  value: item.limitval
-                });
-                return sum;
-              }, []);
-              obj.item.options = arr;
-            }
-            // 多状态合并的select
-            if (current.conds && current.conds.length > 0) {
-              let sumArray = [];
-              current.conds.map((item) => {
-                sumArray = sumArray.concat(item.combobox.reduce((sum, temp) => {
-                  sum.push({
-                    label: temp.limitdesc,
-                    value: `${item.colname}|${temp.limitval}`
-                  });
-                  return sum;
-                }, []));
-                return item;
-              });
-              obj.item.options = sumArray;
-            }
-
-            //check
-            if (current.display === 'check') {
-              obj.item.props.type = 'checkbox';
-            }
-            // textarea
-            if (current.display === 'textarea') {
-              obj.item.props.type = 'textarea';
-            }
-            // 日期控件属性控制
-            if (current.display === 'OBJ_DATENUMBER') {
-              obj.item.props.type = 'date';
-            }
-            if (current.display === 'OBJ_DATE') {
-              obj.item.props.type = 'date';
-            }
-            if (current.display === 'OBJ_TIME') {
-              obj.item.props.type = 'date';
-            }
-
-            // 属性isuppercase控制
-            if (current.isuppercase) {
-
-            }
-            if (current.display === 'OBJ_FK') {
-              switch (current.fkobj.searchmodel) {
-                case 'drp':
-                  obj.item.props.single = true;
-                  obj.item.props.defaultSelected = this.defaultValue(current);
-                  break;
-                case 'mrp':
-                  obj.item.props.single = false;
-                  obj.item.props.defaultSelected = this.defaultValue(current);
-                  break;
-                case 'pop':
-                  obj.item.props.fkobj = current.fkobj;
-                  obj.item.props.Selected = [];
-                  break;
-                case 'mop':
-                  obj.item.props.fkobj = current.fkobj;
-                  obj.item.props.datalist = [];
-                  obj.item.props.Selected = [];
-                  break;
-                default: break;
+        let items = [];
+        if(this.type && Object.prototype.hasOwnProperty.call(this.defaultData,'addcolums')){
+          items = this.defaultData.addcolums.reduce((array, current, itemIndex) =>{
+            let tem = [];
+            if(Object.prototype.hasOwnProperty.call(current,'childs') ){
+              tem = current.childs.reduce((array2, current2, itemIndex2) =>{
+                  let option = this.reduceForm(array2, current2, itemIndex2);
+                  array2.push(option);
+                  return array2;
+                },[]);
               }
-            }
-                  array.push(obj);
-                  return array;
+            array.push({
+              childs:tem,
+              hrdisplay:current.hrdisplay,
+              parentdesc:current.parentdesc,
+              parentname:current.parentname
+            });
+            return array;
           },[]);
-          return items
+          this.newdefaultData = items;
+          this.newdefaultData.objviewcol = this.defaultData.objviewcol;
         }
+        return this.newdefaultData;
       }
     },
     methods:{
+      CollapseClose(index,hrdisplay){
+        this.newdefaultData[index].hrdisplay = '';
+      },
+      reduceForm(array, current, itemInde){
+        let obj = {} ;
+        obj.row = current.row ? current.row : 1;
+        obj.col = current.col ? current.col : 1;
+        obj.component = ItemComponent;
+        obj.item = {
+          type: this.checkDisplay(current),
+          title: current.name,
+          field: current.colname,
+          value: this.defaultValue(current),
+          inputname: current.inputname,
+          props: {...current},
+          event: {},
+          validate: {
+          }
+        };
+        // 属性赋值
+        this.propsType(current,obj.item);
+
+        return obj;
+      },
       checkDisplay(item) {
         let str = '';
         if (!item.display || item.display === 'text' || item.display === 'textarea') {
@@ -175,8 +121,8 @@
         if (item.display === 'check') {
           str = 'checkbox';
         }
-        if (item.display === 'OBJ_FK') {
-          switch (item.fkobj.searchmodel) {
+        if (item.display === 'text') {
+          switch (item.fkdisplay) {
             case 'drp':
               str = 'DropDownSelectFilter';
               break;
@@ -204,26 +150,109 @@
         return str;
       },
       defaultValue(item) { // 设置表单的默认值
-        if(item.valuedat === 'N'){
+        if(item.valuedata === 'N'){
           return false;
-        }else if(item.valuedat === 'Y'){
+        }else if(item.valuedata === 'Y'){
           return true;
         }else {
           return item.valuedata;
         }
       },
-      propsType(){
+      propsType(current,item){
+        // input
+        if (!item.display || item.display === 'text') {
+              item.props.type = 'text';
+              if( item.display === 'textarea' ){
+                item.props.type = 'textarea';
+              }
+              if( current.isnotnull === true){
+                item.required = true;
+              }
+
+        }
         // 外键的单选多选判断
 
+        if (current.combobox) {
+          const arr = current.combobox.reduce((sum, item) => {
+            sum.push({
+              label: item.limitdesc,
+              value: item.limitval
+            });
+            return sum;
+          }, []);
+          item.options = arr;
+          return item;
+        }
+        // 多状态合并的select
+        if (current.conds && current.conds.length > 0) {
+          let sumArray = [];
+          current.conds.map((item) => {
+            sumArray = sumArray.concat(item.combobox.reduce((sum, temp) => {
+              sum.push({
+                label: temp.limitdesc,
+                value: `${item.colname}|${temp.limitval}`
+              });
+              return sum;
+            }, []));
+            return item;
+          });
+          obj.item.options = sumArray;
+          return item;
+        }
 
+        //check
+        if (current.display === 'check') {
+          item.props.type = 'checkbox';
+        }
+        // textarea
+        if (current.display === 'textarea') {
+          item.props.type = 'textarea';
+        }
+        // 日期控件属性控制
+        if (current.display === 'OBJ_DATENUMBER') {
+          item.props.type = 'date';
+        }
+        if (current.display === 'OBJ_DATE') {
+            item.props.type = 'date';
+        }
+        if (current.display === 'OBJ_TIME') {
+            item.props.type = 'date';
+        }
 
+        // 属性isuppercase控制
+        if (current.isuppercase) {
+
+        }
+        if (current.display === 'OBJ_FK') {
+          switch (current.fkobj.searchmodel) {
+            case 'drp':
+              item.props.single = true;
+              item.props.defaultSelected = this.defaultValue(current);
+              break;
+            case 'mrp':
+              item.props.single = false;
+              item.props.defaultSelected = this.defaultValue(current);
+              break;
+            case 'pop':
+              item.props.fkobj = current.fkobj;
+              item.props.Selected = [];
+              break;
+            case 'mop':
+              item.props.fkobj = current.fkobj;
+              item.props.datalist = [];
+              item.props.Selected = [];
+              break;
+            default: break;
+          }
+        }
+        return item;
       }
-    },
+  },
     mounted() {
 
     },
     created() {
-          //console.log(this.computdefaultData,this.type);
+
     },
   }
 </script>
