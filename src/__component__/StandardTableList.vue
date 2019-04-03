c04645ed83ee4f1f2228f5692538c040f3601420
? <843748862.com>
2019-04-03 11:17:27 +0800

    修改列表查询的日期控制，增加埋点功能测试

--- src/__component__/StandardTableList.vue
+++ src/__component__/StandardTableList.vue
@@ -700,8 +700,12 @@
           let value = '';
           this.formItemsLists.every((temp) => {
             if (temp.item.field === item) { // 等于当前节点，判断节点类型
-              if (temp.item.type === 'DatePicker' && (temp.item.props.type === 'datetimerange' || temp.item.props.type === 'daterange') && (jsonData[item][0] && jsonData[item][1])) { // 当为日期控件时，数据处理
-                value = jsonData[item].join('~');
+              if (temp.item.type === 'DatePicker' && (temp.item.props.type === 'datetimerange' || temp.item.props.type === 'daterange')) { // 当为日期控件时，数据处理
+                if ((jsonData[item][0] && jsonData[item][1])) {
+                  value = jsonData[item].join('~');
+                } else {
+                  value = '';
+                }
                 return false;
               }
 
