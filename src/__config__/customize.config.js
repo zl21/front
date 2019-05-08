3cc695b8150e50ab39a442dbf7324745182e3fba
HuaiYang <hycool.happy@163.com>
2019-05-08 16:16:29 +0800

    移除自定义界面无效代码

--- src/__config__/customize.config.js
+++ src/__config__/customize.config.js
@@ -1,7 +1,5 @@
 export default {
   DingTalkModuleConfig: {
-    // 自定义界面：掌云数据报表权限设置模块
-    path: 'DingTalkModuleConfig',
     component: () => import('../__customize__/palmCloud/PowerConfig.vue')
   },
 };
