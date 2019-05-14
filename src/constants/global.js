877cbcc80b160986ad8f9d827e9e144c8bdfb8a7
liu.li <li.liu@burgeon.cn>
2019-05-14 17:12:42 +0800

    判断网关关闭

--- src/constants/global.js
+++ src/constants/global.js
@@ -8,12 +8,12 @@ export const HORIZONTAL_TABLE_DETAIL_COMPONENT_PREFIX = 'H';
 export const VERTICAL_TABLE_DETAIL_COMPONENT_PREFIX = 'V';
 export const CUSTOMIZED_MODULE_COMPONENT_PREFIX = 'C';
 
-export const enableGateWay = true;
 
 export const KEEP_MODULE_STATE_WHEN_CLICK_MENU = true; // 控制点击按钮菜单时如何显示已经打开的模块
 export const KEEP_SAVE_ITEM_TABLE_MANDATORY = true; // 控制当子表没有必填项点击保存时是否必须填写子表信息
 export const Version = '1.4'; // 当前版本是1.4
 export const VersionName = 'qiaodan'; // 当前版本是否是乔丹版本
+export const enableGateWay = Version === '1.4';
 
 // 需要走全局公共网关的接口
 export const globalGateWay = [
