56404b1d365b3c058d77f0d7597d52bd198b8b7f
liu.li <li.liu@burgeon.cn>
2019-05-14 18:04:40 +0800

    form 版本文件

---
+++ src/constants/formHttpRequest/version_1.4/fkHttpRequest.js
@@ -0,0 +1,46 @@
+
+import network, { urlSearchParams } from '../../../__utils__/network';
+
+export const fkQueryList = function fkQueryList(params) {
+  network.post('/p/cs/QueryList', urlSearchParams({ searchdata: params.searchObject }), { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res);
+    }
+  });
+};
+
+export const fkFuzzyquerybyak = function fkFuzzyquerybyak(params) {
+  network.post('/p/cs/fuzzyquerybyak', urlSearchParams(params.searchObject), { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res);
+    }
+  });
+};
+export const fkGetMultiQuery = function fkGetMultiQuery(params) {
+  network.post('/p/cs/getMultiQuery', urlSearchParams(params.searchObject), { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res.data.data);
+    }
+  });
+};
+export const fkDelMultiQuery = function fkDelMultiQuery(params) {
+  network.post('/p/cs/delMultiQuery', urlSearchParams(params.searchObject), { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res);
+    }
+  });
+};
+export const fkQueuploadProgressry = function fkQueuploadProgressry(params) {
+  network.post('/p/cs/uploadProgress', urlSearchParams(params.searchObject), { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res);
+    }
+  });
+};
+export const fkObjectSave = function fkObjectSave(params) {
+  network.post('/p/cs/objectSave', params.searchObject, { serviceId: params.serviceId }).then((res) => {
+    if (typeof params.success === 'function') {
+      params.success(res);
+    }
+  });
+};
