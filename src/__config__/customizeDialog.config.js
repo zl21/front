a3fe49264ce095255850d42211c188d82a77b73a
xia.hj <xia.hj@burgeon.cn>
2019-05-28 15:14:12 +0800

    增加按钮定制弹框配置文件

---
+++ src/__config__/customizeDialog.config.js
@@ -0,0 +1,8 @@
+export default {
+  clonePopUp: {
+    omponentName: () => import('../__customize__/palmCloud/PowerConfig.vue')
+  },
+  functionPermission: {
+    omponentName: () => import('../__customize__/functionPower/FunctionPower.vue')
+  },
+};
