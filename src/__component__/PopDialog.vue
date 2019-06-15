2ad08773a8018e5cdc0390c0e9d6595b5adb38d3
liu.li <li.liu@burgeon.cn>
2019-06-15 14:01:33 +0800

    form 弹窗单选 筛选条件呢

--- src/__component__/PopDialog.vue
+++ src/__component__/PopDialog.vue
@@ -17,9 +17,11 @@
     <component
       :is="'CompositeForm'"
       v-if="formList.show"
+      ref="CompositeForm"
       :default-data="formList"
       :default-column-col="formList.objviewcol"
       class="formPanel"
+      :Condition = "Condition"
       @InitializationForm="InitializationForm"
       @formChange="formChange"
       @on-formEnter="searchForm"
@@ -69,240 +71,252 @@
   </div>
 </template>
 <script>
-  import { Version } from '../constants/global';
+import { Version } from "../constants/global";
 
+// eslint-disable-next-line import/no-dynamic-require
+const {
+  getTableQuery,
+  fkQueryListPop
   // eslint-disable-next-line import/no-dynamic-require
-  const {
-    getTableQuery, fkQueryListPop
-  // eslint-disable-next-line import/no-dynamic-require
-  } = require(`../__config__/actions/version_${Version}/formHttpRequest/fkHttpRequest.js`);
+} = require(`../__config__/actions/version_${Version}/formHttpRequest/fkHttpRequest.js`);
 
-  export default {
-    name: 'PopDialog',
-    components: {
-    },
-    data() {
-      return {
-        formList: {
-          inpubobj: [],
-          show: false,
-          objviewcol: 4
-        },
-        newformList: {},
-        parms:{},
-        formChangeData: {},
-        SelectionData: {
-          config: [],
-          thead: [],
-          row: [],
-          item: '',
-          tableAllDatas: {},
-        },
-        SingleSelect: {
-          show: false,
-        },
-        loading: false,
-        params: {},
-        selectOperation: { // 列表的分页
-          currentPageIndex: 1, // 当前页码
-          pageSize: 10, // 显示条数
-          defaultrange: 10, // 分页下拉选中值
-          totalRowCount: 0,
-          startindex: 0,
-          selectrange: [10, 20, 50, 100, 200]
-        },
-        ids: []
-      };
-    },
-    props: {
-      fkobj: {
-        type: Object,
-        default() {
-          return {};
-        }
-      }
-    },
-    created() {
-      this.loading = true;
-      const router = this.$route.params;
-      const params = {
-        tableid: this.fkobj.reftableid,
-        getcmd: 'n',
-        table: router.tableName
-      };
-      this.params = params;
-      this.getData(params);
-    },
-    mounted() {
-      
-    },
-    watch: {
+export default {
+  name: "PopDialog",
+  components: {},
+  data() {
+    return {
       formList: {
-        handler(val) {
-          this.newformList = Object.assign(this.newformList,val);
-        },
-        deep: true
-
-      }
-    },
-    methods: {
-      rowdblclick(parms){
-        this.parms = parms;
-         this.$parent.ok();
+        inpubobj: [],
+        show: false,
+        objviewcol: 4
       },
-      getData(searchObject) {
-        //  form 请求
-        getTableQuery({
-          searchObject,
-          success: (res) => {
-            if (res.data.code === 0) {
-              this.loading = false;
-              const Data = res.data.data.datas;
-              Data.dataarry.forEach((item) => {
-                item.name = item.coldesc;
-                item.defval = item.default;
-                if (item.fkobj) {
-                  item.data = {};
-                }
-              });
-              this.formList.inpubobj = Data.dataarry;
-              this.formList.show = true;
-              this.formList.objviewcol = Data.searchFoldnum;
-              this.getList();
-            }
-          }
-        });
+      newformList: {},
+      Condition: "list",
+      parms: {},
+      formChangeData: {},
+      formChangeDataInt:{}, // 初始化
+      SelectionData: {
+        config: [],
+        thead: [],
+        row: [],
+        item: "",
+        tableAllDatas: {}
       },
-      cancelDialog() {
-        this.$parent.cancel();
+      SingleSelect: {
+        show: false
       },
-      getList() {
-        //  table list 请求
-        const searchObject = {
-          refcolid: Number(this.fkobj.colid),
-          isdroplistsearch: true,
-          startindex: this.selectOperation.startindex,
-          range: this.selectOperation.pageSize
-        };
-        if (Object.keys(this.formChangeData).length > 0) {
-          searchObject.fixedcolumns = { ...this.formChangeData };
-        }
-        fkQueryListPop({
-          searchObject,
-          success: (res) => {
-            if (res.data.code === 0) {
-              const data = res.data.data;
-              this.selectOperation.currentPageIndex = data.selectrange; // 当前页码
-              // this.selectOperation.pageSize = data.rowCount; // 显示条数
-              this.selectOperation.totalRowCount = data.totalRowCount;
-              this.selectOperation.selectrange = data.selectrange;
-              this.selectOperation.defaultrange = data.defaultrange;
-              // this.selectOperation.startindex = data.start;
-              this.SelectionData.thead = data.ordids.reduce((arr, item) => {
-                const title = data.tabth.find(x => x.colname === item.colname).name;
-                if(title === 'ID'){
-                    arr.unshift({
-                      title: '序号',
-                      key: `${item.colname}`,
-                      render: (h, params) => h('div',
-                          {
-                            domProps: {
-                              innerHTML: item.colname === 'ID' ? data.start + params.index + 1 : params.row[item.colname].val
-                            }
-                      })
-                    });
-
-                }else{
-                  arr.push({
-                  title: title,
-                  key: `${item.colname}`,
-                  render: (h, params) => h('div',
-                      {
-                        domProps: {
-                          innerHTML: params.row[item.colname].val
-                        }
-                      })
-                });
-
-                }
-                return arr;
-              }, []);
-              this.SelectionData.row = data.row.concat([]);
-            }
-          }
-        });
+      loading: false,
+      params: {},
+      selectOperation: {
+        // 列表的分页
+        currentPageIndex: 1, // 当前页码
+        pageSize: 10, // 显示条数
+        defaultrange: 10, // 分页下拉选中值
+        totalRowCount: 0,
+        startindex: 0,
+        selectrange: [10, 20, 50, 100, 200]
       },
-      searchForm() {
-        this.getList();
-      },
-      saveData() {
-        
-      },
-      pageChange(index) {
-        this.selectOperation.startindex = (index - 1) * this.selectOperation.pageSize;
-
-        this.getList();
-      },
-      pageSizeChange(index) {
-        this.selectOperation.startindex = index * this.selectOperation.pageSize;
-        this.selectOperation.defaultrange = index;
-        this.getList();
-      },
-      InitializationForm(data) {
-        // 默认
-        if (Object.keys(data).length > 0) {
-          Object.keys(data).forEach((item) => {
-            if (data[item] !== undefined) {
-              const dataSelect = this.checkForm(data, item);
-              this.formChangeData = Object.assign(this.formChangeData, { [item]: dataSelect[item] });
-            }
-          });
-        }
-        this.getList();
-      },
-      checkForm(data, item) {
-        // 校验select
-        const index = this.formList.inpubobj.findIndex(x => x.colname === item);
-        if (index !== -1 && this.formList.inpubobj[index] && this.formList.inpubobj[index].display) {
-          if (this.formList.inpubobj[index].display === 'OBJ_SELECT' || this.formList.inpubobj[index].display === 'select') {
-            data[item] = [`=${data[item]}`.toString()];
-          }
-        }
-        return data;
-      },
-      formChange(data) {
-        // form 修改的数据
-        const nameKeys = Object.keys(data)[0].split(':');
-        if (nameKeys.length > 0) {
-          delete this.formChangeData[nameKeys[0]];
-          delete this.formChangeData[`${nameKeys[0]}:NAME`];
-        }
-        const dataSelect = this.checkForm(data, Object.keys(data)[0]);
-        this.formChangeData = Object.assign(this.formChangeData, dataSelect);
-      },
-      confirm() {
-        // b保存提交
-        this.saveData();
+      ids: []
+    };
+  },
+  props: {
+    fkobj: {
+      type: Object,
+      default() {
+        return {};
       }
     }
-  };
+  },
+  created() {
+    this.loading = true;
+    const router = this.$route.params;
+    const params = {
+      tableid: this.fkobj.reftableid,
+      getcmd: "n",
+      table: router.tableName
+    };
+    this.params = params;
+    this.getData(params);
+  },
+  mounted() {
+    //this.$refs.CompositeForm.mountChecked = true;
+  },
+  watch: {
+    formList: {
+      handler(val) {
+        this.newformList = Object.assign(this.newformList, val);
+      },
+      deep: true
+    }
+  },
+  methods: {
+    rowdblclick(parms) {
+      this.parms = parms;
+      this.$parent.ok();
+    },
+    getData(searchObject) {
+      //  form 请求
+      getTableQuery({
+        searchObject,
+        success: res => {
+          if (res.data.code === 0) {
+            this.loading = false;
+            const Data = res.data.data.datas;
+            Data.dataarry.forEach(item => {
+              item.name = item.coldesc;
+              item.defval = item.default;
+              if (item.fkobj) {
+                item.data = {};
+              }
+            });
+            this.formList.inpubobj = Data.dataarry;
+            this.formList.show = true;
+            this.formList.objviewcol = Data.searchFoldnum;
+
+            // setTimeout(() => {
+            //   // if (this.$refs.CompositeForm) {
+            //   //   this.$refs.CompositeForm.mountChecked = true;
+            //   // }
+            // }, 100);
+            this.getList();
+          }
+        }
+      });
+    },
+    cancelDialog() {
+      this.$parent.cancel();
+    },
+    getList() {
+      //  table list 请求
+      const searchObject = {
+        refcolid: Number(this.fkobj.colid),
+        isdroplistsearch: true,
+        startindex: this.selectOperation.startindex,
+        range: this.selectOperation.pageSize
+      };
+      if (Object.keys(this.formChangeData).length > 0) {
+        searchObject.fixedcolumns = { ...this.formChangeData };
+      }
+      fkQueryListPop({
+        searchObject,
+        success: res => {
+          if (res.data.code === 0) {
+            const data = res.data.data;
+            this.selectOperation.currentPageIndex = data.selectrange; // 当前页码
+            // this.selectOperation.pageSize = data.rowCount; // 显示条数
+            this.selectOperation.totalRowCount = data.totalRowCount;
+            this.selectOperation.selectrange = data.selectrange;
+            this.selectOperation.defaultrange = data.defaultrange;
+            // this.selectOperation.startindex = data.start;
+            this.SelectionData.thead = data.ordids.reduce((arr, item) => {
+              const title = data.tabth.find(x => x.colname === item.colname)
+                .name;
+              if (title === "ID") {
+                arr.unshift({
+                  title: "序号",
+                  key: `${item.colname}`,
+                  render: (h, params) =>
+                    h("div", {
+                      domProps: {
+                        innerHTML:
+                          item.colname === "ID"
+                            ? data.start + params.index + 1
+                            : params.row[item.colname].val
+                      }
+                    })
+                });
+              } else {
+                arr.push({
+                  title: title,
+                  key: `${item.colname}`,
+                  render: (h, params) =>
+                    h("div", {
+                      domProps: {
+                        innerHTML: params.row[item.colname].val
+                      }
+                    })
+                });
+              }
+              return arr;
+            }, []);
+            this.SelectionData.row = data.row.concat([]);
+          }
+        }
+      });
+    },
+    searchForm() {
+      this.getList();
+    },
+    saveData() {},
+    pageChange(index) {
+      this.selectOperation.startindex =
+        (index - 1) * this.selectOperation.pageSize;
+
+      this.getList();
+    },
+    pageSizeChange(index) {
+      this.selectOperation.startindex = index * this.selectOperation.pageSize;
+      this.selectOperation.defaultrange = index;
+      this.getList();
+    },
+    InitializationForm(data) {
+      // 默认
+      if (Object.keys(data).length > 0) {
+        Object.keys(data).forEach(item => {
+          if (data[item] !== undefined) {
+            const dataSelect = this.checkForm(data, item);
+            this.formChangeDataInt = Object.assign(this.formChangeData, {
+              [item]: dataSelect[item]
+            });
+          }
+        });
+      }
+      this.getList();
+    },
+    checkForm(data, item) {
+      // 校验select
+      const index = this.formList.inpubobj.findIndex(x => x.colname === item);
+      if (
+        index !== -1 &&
+        this.formList.inpubobj[index] &&
+        this.formList.inpubobj[index].display
+      ) {
+        if (
+          this.formList.inpubobj[index].display === "OBJ_SELECT" ||
+          this.formList.inpubobj[index].display === "select"
+        ) {
+          data[item] = [`=${data[item]}`.toString()];
+        }
+      }
+      return data;
+    },
+    formChange(data) {
+      // form 修改的数据
+       this.formChangeData = data;
+    },
+    confirm() {
+      // b保存提交
+      this.saveData();
+    }
+  }
+};
 </script>
 <style lang="less" scope>
 .modify-tip {
-    display: inline-block;
-    margin-left: 20px;
-    font-size: 12px;
-    margin: 0px 0 10px;
-    height: 24px;
-    line-height:24px;
+  display: inline-block;
+  margin-left: 20px;
+  font-size: 12px;
+  margin: 0px 0 10px;
+  height: 24px;
+  line-height: 24px;
 }
-.popDialog_pop{
-  padding:20px
+.popDialog_pop {
+  padding: 20px;
 }
-.pageInfo{
-  padding:10px 0;
-  display:flex;
+.pageInfo {
+  padding: 10px 0;
+  display: flex;
   flex-flow: row nowrap;
- justify-content: space-between;
+  justify-content: space-between;
 }
 </style>
