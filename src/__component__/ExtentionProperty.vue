c3a0fc18eb4dc4058f7a1698db0d6766c560e42c
HuaiYang <hycool.happy@163.com>
2019-06-20 15:11:33 +0800

    完成表、字段的拓展属性建模

---
+++ src/__component__/ExtentionProperty.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="extentionProperty">
+    <div class="left"></div>
+    <div class="middle"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ExtentionProperty'
+  }
+</script>
+
+<style scoped lang="less">
+  .extentionProperty {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    .left {
+      flex: 1;
+      border: 1px solid black;
+    }
+    .middle {
+      flex: 2;
+      border: 1px solid yellow;
+    }
+    .right {
+      flex: 3;
+      border: 1px solid blue;
+    }
+  }
+</style>
---
+++ src/constants/extentionPropertyForTable.js
@@ -0,0 +1,221 @@
+export default [
+  {
+    name: '列表默认排序',
+    key: 'orderby',
+    type: 'object-group',
+    enableIncrease: true,
+    objectInfo: [
+      {
+        name: '字段名称',
+        key: 'column',
+        type: 'input'
+      },
+      {
+        name: '升序',
+        key: 'asc',
+        type: 'radio',
+        enumerateValue: [true, false]
+      }
+    ],
+    description: '请描述一下这个配置有啥作用'
+  },
+  {
+    name: '弹框多选',
+    key: '__root__',
+    type: 'input-group',
+    inputLists: [
+      {
+        name: '全局检索自动匹配字段',
+        key: 'multiCenter',
+        type: 'input',
+        outputValueType: 'Array'
+      },
+      {
+        name: '全局检索字段',
+        key: 'multiIndex',
+        type: 'input',
+        outputValueType: 'Array'
+      },
+      {
+        name: '中间展示字段',
+        key: 'multiColumn',
+        type: 'input',
+        outputValueType: 'Array'
+      },
+      {
+        name: '左侧字段',
+        key: 'multiFilter',
+        type: 'input',
+        outputValueType: 'Array'
+      },
+    ],
+  },
+  {
+    name: '模糊匹配',
+    key: 'fuzzyQuery',
+    type: 'object-group',
+    enableIncrease: true,
+    objectInfo: [
+      {
+        name: '字段名称',
+        key: 'colName',
+        type: 'input'
+      },
+      {
+        name: '模糊匹配方式',
+        key: 'matchType',
+        type: 'input',
+      }
+    ],
+    description: '请描述一下这个配置有啥作用'
+  },
+  {
+    name: '外键下拉',
+    key: 'fkdrplist',
+    type: 'object-combine',
+    enableIncrease: true,
+    objectInfo: [
+      {
+        name: '排序信息',
+        key: 'orderby',
+        type: 'object-group',
+        objectInfo: [
+          {
+            name: '字段名称',
+            key: 'column',
+            type: 'input'
+          },
+          {
+            name: '升序',
+            key: 'asc',
+            type: 'radio',
+            enumerateValue: [true, false]
+          }
+        ]
+      },
+      {
+        name: '下拉弹出显示列',
+        key: 'searchcols',
+        type: 'input',
+        outputValueType: 'Array'
+      }
+    ],
+    description: '请描述一下这个配置有啥作用'
+  },
+  {
+    name: '输入键查询',
+    key: 'ak2',
+    type: 'input'
+  },
+  {
+    name: '导出',
+    key: 'DisableEXPORT',
+    type: 'radio',
+    enumerateValue: [true, false]
+  },
+  {
+    name: '导入',
+    key: 'smalltable',
+    type: 'radio',
+    enumerateValue: [true, false]
+  },
+  {
+    name: '商品数量矩阵配置',
+    key: '__root__',
+    type: 'input-group',
+    inputLists: [
+      {
+        name: '对应字段名1',
+        key: 'pdtcol',
+        type: 'input'
+      },
+      {
+        name: '对应字段名2',
+        key: 'qtycol',
+        type: 'input'
+      },
+      {
+        name: '是否显示数量',
+        key: 'qtyisshow',
+        type: 'radio',
+        enumerateValue: [true, false]
+      },
+      {
+        name: '扫描数量',
+        key: 'defnum',
+        type: 'input',
+        inputType: 'Number'
+      },
+    ]
+  },
+  {
+    name: '按钮显示',
+    key: 'filteracshow',
+    type: 'object-group',
+    objectInfo: [
+      {
+        name: '按钮名称',
+        key: 'ackey',
+        type: 'select',
+        selectOptions: [
+          { text: '新增', value: 'actionADD' },
+          { text: '修改', value: 'actionMODIFY' },
+          { text: '删除', value: 'ctionDELETE' },
+          { text: '提交', value: 'actionSUBMIT' },
+          { text: '取消提交', value: 'actionUNSUBMIT' },
+          { text: '作废', value: 'actionVOID' },
+          { text: '复制', value: 'actionCANCOPY' },
+          { text: '导入', value: 'actionIMPORT' },
+          { text: '导出', value: 'actionEXPORT' },
+          { text: '打印', value: 'actionPRINT' },
+        ]
+      }
+    ]
+  },
+  {
+    name: '只读状态',
+    key: 'pstatus',
+    type: 'input-group',
+    inputLists: [
+      {
+        name: '字段名',
+        key: 'col',
+        type: 'input'
+      },
+      {
+        name: '字段值',
+        key: 'readforvals',
+        type: 'input',
+        outputValueType: 'Array'
+      }
+    ]
+  },
+  {
+    name: '水印图片优先级',
+    key: 'wkprioritys',
+    type: 'input',
+    outputValueType: 'Array'
+  },
+  {
+    name: '列表样式优先级',
+    key: 'status4css_priority',
+    type: 'input'
+  },
+  {
+    name: '下拉列表显示行数',
+    key: 'listrange',
+    type: 'input',
+    inputType: 'Number'
+  },
+  {
+    name: '列表定制查询',
+    key: 'cusQResult',
+    type: 'input-group',
+    inputLists: [
+      {
+        name: '服务名',
+        key: 'action'
+      }
+    ]
+  },
+];
