f1a7c66629588877ecb3cdb259b807256c90cfa3
liu.li <li.liu@burgeon.cn>
2019-06-25 14:29:02 +0800

    弹窗多选样式

--- src/__component__/Navigator.vue
+++ src/__component__/Navigator.vue
@@ -39,34 +39,47 @@
         :index="index"
       />
     </div>
-    <div class="tag right">
-      <i
-        class="iconfont icon-yonghu-"
-        @click="show = true"
-      />
-    </div>
-    <Drawer
-      v-model="show"
-    >
-      <SetPanel
-        :panel="setPanel"
-        @changePwdBox="changePwdBox"
-      />
-    </Drawer>
-    <Dialog
-      ref="dialogRef"
-      :title="dialogConfig.title"
-      :mask="dialogConfig.mask"
-      :content-text="dialogConfig.contentText"
-      :footer-hide="dialogConfig.footerHide"
-      :confirm="dialogConfig.confirm"
-      :dialog-component-name="dialogComponentName"
-    />
-  </div>
+    <div :class="searchBtn ? 'tag right' :'tag tag-search right' ">
+       <template v-if="searchBtn === false">
+         <Poptip trigger="focus">
+            <Input  placeholder="请输入要查询的功能名" @input="searchData" style="width: auto" class="nav-search">
+            <Icon type="ios-search" slot="suffix" @click="searchBtn = true" />
+            </Input>
+            <div slot="content" v-if ="searchList.length>0">
+              <div class="Poptip-nav" style="color:#000">
+                <ul>
+                    <li v-for="(item,index) in searchList"  @click="routerNext(index)">
+                      {{item.desc}}
+                    </li>
+              </ul>
+              </div>
+              
+              
+            </div>
+        </Poptip>
+         
+
+       </template>
+
+
+<i v-if="searchBtn === true" class="iconfont icon-kuangjia-sousuo" @click="searchBtn = false" />
+</div>
+<div class="tag right">
+    <i class="iconfont icon-yonghu-" @click="show = true" />
+</div>
+<Drawer v-model="show">
+    <SetPanel :panel="setPanel" @changePwdBox="changePwdBox" />
+</Drawer>
+<Dialog ref="dialogRef" :title="dialogConfig.title" :mask="dialogConfig.mask" :content-text="dialogConfig.contentText" :footer-hide="dialogConfig.footerHide" :confirm="dialogConfig.confirm" :dialog-component-name="dialogComponentName" />
+</div>
 </template>
 
 <script>
-  import { mapState, mapMutations, mapActions } from 'vuex';
+  import {
+    mapState,
+    mapMutations,
+    mapActions
+  } from 'vuex';
   import Vue from 'vue';
   import NavigatorPrimaryMenu from './NavigatorPrimaryMenu';
   import SetPanel from './SetPanel';
@@ -76,7 +89,13 @@
   import openedImg from '../assets/image/open@2x.png';
   import logoImg from '../assets/image/logo.png';
   import bannerImg from '../assets/image/banner.png';
-  
+  import {
+    enableGateWay
+  } from '../constants/global';
+  import network, {
+    urlSearchParams
+  } from '../__utils__/network';
+
   export default {
     name: 'Navigator',
     components: {
@@ -93,6 +112,8 @@
           bannerImg
         },
         show: false,
+        searchBtn: true,
+        searchList: [],
         setPanel: {
           show: true,
           list: [],
@@ -102,16 +123,19 @@
           mask: true,
           footerHide: false,
           contentText: '',
-          confirm: () => {
-          }
+          confirm: () => {}
         }, // 弹框配置信息
         dialogComponentName: null
       };
     },
     computed: {
       ...mapState('global', {
-        collapseHistoryAndFavorite: ({ collapseHistoryAndFavorite }) => collapseHistoryAndFavorite,
-        menuLists: ({ menuLists }) => menuLists
+        collapseHistoryAndFavorite: ({
+          collapseHistoryAndFavorite
+        }) => collapseHistoryAndFavorite,
+        menuLists: ({
+          menuLists
+        }) => menuLists
       })
     },
     methods: {
@@ -124,6 +148,19 @@
         this.dialogConfig.footerHide = true;
         Vue.component('ChangePassword', CustomizeModule.ChangePassword.component);
         this.dialogComponentName = 'ChangePassword';
+      },
+      searchData(value) {
+        const globalServiceId = window.sessionStorage.getItem('serviceId');
+        network.post('/p/cs/SearchWords', urlSearchParams({
+          words: value,
+        })).then((r) => {
+          if (r.status === 200 && r.data.code === 0) {
+            this.searchList = r.data.data;
+          }
+        });
+      },
+      routerNext(value){
+          console.log(value);
       },
       loadEnterpriseConfig() {
         const image = (window.ProjectConfig || {}).image || {
@@ -144,64 +181,110 @@
 </script>
 
 <style lang="less">
- .burgeon-drawer-content{//重置BurgeonUI样式
-    border-top-left-radius: 0px !important; 
-    border-top-right-radius: 0px !important; 
-  }
-  .burgeon-drawer-body{//重置BurgeonUI样式
-    padding: 0px !important; 
-  }
-  .navigator {
-    height: 100%;
-    display: flex;
-    background-color: #1f272c;
-    .left {
-      img.trigger {
-        height: 50px;
-      }
-      img.logo {
-        position: absolute;
-        width: 30px;
-        top: 10px;
-        left: 18px;
-      }
-      img.banner {
-        width: 76px;
-        height: 30px;
-        position: absolute;
-        top: 11px;
-        left: 64px;
-      }
-      img:hover {
-        cursor: pointer;
-      }
+    .burgeon-drawer-content {
+        //重置BurgeonUI样式
+        border-top-left-radius: 0px !important;
+        border-top-right-radius: 0px !important;
     }
-    .middle {
-      margin-left: 10px;
-      position: relative;
-      display: flex;
-      flex: 1 1 1px;
+
+    .burgeon-drawer-body {
+        //重置BurgeonUI样式
+        padding: 0px !important;
     }
-    .tag {
-      width: 50px;
-      float: left;
-      font-size: 24px;
-      text-align: center;
-      line-height: 50px;
-      cursor: pointer;
-      color: #fff;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-      i{
-        font-size: 22px;
-      }
+
+    .navigator {
+        height: 100%;
+        display: flex;
+        background-color: #1f272c;
+        .left {
+            img.trigger {
+                height: 50px;
+            }
+            img.logo {
+                position: absolute;
+                width: 30px;
+                top: 10px;
+                left: 18px;
+            }
+            img.banner {
+                width: 76px;
+                height: 30px;
+                position: absolute;
+                top: 11px;
+                left: 64px;
+            }
+            img:hover {
+                cursor: pointer;
+            }
+        }
+        .middle {
+            margin-left: 10px;
+            position: relative;
+            display: flex;
+            flex: 1 1 1px;
+        }
+        .nav-search {
+            input {
+                display: inline-block;
+                width: 100%;
+                padding: 0 8px;
+                border: solid 1px #fff;
+                border-radius: 15px;
+                background: #4f5356;
+                height: 28px;
+                line-height: 28px;
+                color: #fff;
+                font-size: 13px;
+            }
+            i {
+                color: #C0C4CC;
+                padding-top: 2px;
+            }
+           
+        }
+        .tag {
+            width: 50px;
+            float: left;
+            font-size: 24px;
+            text-align: center;
+            line-height: 50px;
+            cursor: pointer;
+            color: #fff;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            i {
+                font-size: 22px;
+            }
+        }
+        .tag-search {
+            width: 192px;
+            line-height: 40px;
+        }
+        .tag:hover {
+            background: #2e373c;
+        }
     }
-    .tag:hover {
-      background: #2e373c;
-    }
-   
-  }
- 
+    .Poptip-nav {
+       ul{
+         li{
+           &:hover{
+                  background: #f4f4f4;
+            }
+              padding: 0 20px;
+              text-align: left;
+              margin: 0;
+              line-height: 34px;
+              cursor: pointer;
+              color: #606266;
+              font-size: 14px;
+              list-style: none;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+            
+       }
+     }
 </style>
