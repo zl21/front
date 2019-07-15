5b6c756e49012ada9ebeebf66539054d4d3e695e
xia.hj <xia.hj@burgeon.cn>
2019-07-15 14:31:23 +0800

    增加单对象界面左右结构编辑保存逻辑

---
+++ src/__config__/mutations/version_1.3/horizontalTableDetail.mutations.js
@@ -0,0 +1,272 @@
+import router from '../../router.config';
+
+export default {
+  updateTabPanelsData(state, data) {
+    const { tableName, tableId } = router.currentRoute.params;
+    const arr = [{
+      label: '标签',
+      tablename: tableName,
+      id: tableId,
+      componentAttribute: {
+        buttonsData: {
+          isShow: true,
+          data
+        },
+        formData: {
+          isShow: false,
+          data: {}
+        },
+        panelData: {
+          isShow: true,
+          data: {}
+        },
+        tableData: {
+          isShow: false,
+          data: {}
+        }
+      }
+    }];
+    data.reftabs.sort((a, b) => a.order - b.order);
+    data.reftabs.forEach((item) => {
+      const obj = { ...item };
+      obj.label = item.tabledesc;
+      obj.componentAttribute = {
+        buttonsData: {
+          isShow: true,
+          data: {}
+        },
+        formData: {
+          isShow: false,
+          data: {}
+        },
+        panelData: {
+          isShow: false,
+          data: {}
+        },
+        tableData: {
+          isShow: false,
+          data: {}
+        }
+      };
+      obj.tableSearchData = {
+        selectedValue: '',
+        inputValue: ''
+      }; // 表格搜索的数据
+      arr.push(obj);
+    });
+    arr.forEach((item) => {
+      state.updateData[item.tablename] = {
+        add: Object.assign({}, { [item.tablename]: {} }),
+        modify: Object.assign({}, { [item.tablename]: {} }),
+        delete: Object.assign({}, { [item.tablename]: {} }),
+        addDefault: {},
+        default: {},
+        checkedInfo: {},
+        changeData: Object.assign({}, state.updateData[item.tablename] ? state.updateData[item.tablename].changeData : {}) // 表单修改的值，第二次回显用
+      };
+    });
+    state.tabPanels = arr;
+  }, // 更新按钮数据
+  updateButtonsData(state, data) {
+    const { componentAttribute } = state.tabPanels[data.tabIndex];
+    componentAttribute.buttonsData.isShow = true;
+    componentAttribute.buttonsData.data = data;
+  }, // 更新按钮数据
+  updateFormData(state, data) {
+    const { componentAttribute } = state.tabPanels[data.tabIndex];
+    if (data.inpubobj) {
+      componentAttribute.formData.isShow = true;
+    }
+    componentAttribute.formData.data = data;
+  }, //
+
+  updatePanelData(state, data) {
+    const { componentAttribute } = state.tabPanels[data.tabIndex];
+    componentAttribute.panelData.isShow = true;
+    componentAttribute.panelData.data = data;
+  }, // 更新form数据
+  updateTableData(state, data) {
+    const { componentAttribute } = state.tabPanels[data.tabIndex];
+    componentAttribute.tableData.isShow = true;
+    componentAttribute.tableData.data = data;
+  }, // 更新表格数据
+  updateTabCurrentIndex(state, index) {
+    state.tabCurrentIndex = index;
+  }, // 更新当前tab的索引
+  updateDefaultData(state, data) {
+    state.updateData[data.tableName].default = data.value;
+  },
+  updateAddData(state, data) {
+    if (Object.values(data.value).length === 0) {
+      state.updateData[data.tableName].add[data.tableName] = {};
+    } else {
+      state.updateData[data.tableName].add[data.tableName] = Object.assign(state.updateData[data.tableName].add[data.tableName], data.value[data.tableName]);
+    }
+  },
+  
+  updateModifyData(state, data) {
+    state.updateData[data.tableName].modify = data.value;
+  },
+  updateAddDefaultData(state, data) {
+    state.updateData[data.tableName].addDefault = data.value;
+  },
+  updateDeleteData(state, data) {
+    if (Object.values(data.value).length === 0) {
+      state.updateData[data.tableName].delete[data.tableName] = data.value;
+    } else {
+      state.updateData[data.tableName].delete = data.value;
+    }
+  },
+  updateChangeData(state, data) {
+    data = JSON.parse(JSON.stringify(data));
+    state.updateData[data.tableName].changeData = Object.assign(data.value, {});
+    state.updateData = Object.assign({}, state.updateData);
+  },
+  updateCheckedInfoData(state, data) {
+    state.updateData[data.tableName].checkedInfo = data.value;
+  },
+  updateNewMainTableAddSaveData(state, { data, itemName }) { // 主表新增保存返回信息
+    const NewObject = Object.keys(data.data).reduce((newData, key) => {
+      console.log(newData);
+      console.log(key);
+      if (key === 'objid') {
+        const keys = 'objId';
+        newData[keys] = data.data[key];
+      } else {
+        newData[key] = data.data[key];
+      }
+
+      // newData = 'objId';
+      // newData[objid] = state.buttonsData.newMainTableSaveData[key];
+      return newData;
+    }, {});
+    state.buttonsData.newMainTableSaveData = NewObject;
+
+    console.log(33, NewObject);
+    state.buttonsData.message = data.message;
+  },
+  updateNewMainTableDeleteData(state, data) { // 删除返回信息
+    state.buttonsData.deleteData = data.message;
+  },
+  updateFormDataForRefshow(state) { // 去除子表缓存
+    const { componentAttribute } = state.tabPanels[state.tabCurrentIndex];
+    componentAttribute.formData.isShow = false;
+  },
+  updateTablePageInfo(state, data) { //  更改列表分页数据
+    state.tablePageInfo = data;
+  },
+  changeCopy(state, data) {
+    state.copy = data;
+  },
+  copyDefaultData(state, { tableName }) { // 执行按钮复制操作重新给form赋值
+    // state.tabPanels.forEach((item) => {
+    //   if (item.tablename === tableName) {
+    state.tabPanels[0].componentAttribute.panelData = Object.assign(state.tabPanels[0].componentAttribute.panelData, state.defaultForCopyData);
+    //   }
+    // });
+  },
+  // changeFormDataForCopy(state, { defaultForCopyDatas, tableName }) {
+  //   state.updateData[tableName].add = defaultForCopyDatas;
+  // },
+  savaCopyData(state, { copyDatas, tableName, modifyData }) { // 执行按钮复制操作存储form默认值数据
+    // state.defaultDataForCopy = copyData;
+    const copySaveDataForParam = {};// 整合changeData所需数据格式
+    state.copyDataForReadOnly.addcolums.forEach((d) => { // 复制按钮操作时江接口请求回来的配置信息赋值给form
+      copyDatas.data.addcolums.forEach((item) => {
+        d.childs.forEach((c) => {
+          item.childs.forEach((b) => {
+            if (b.name === c.name) {
+              b.readonly = c.readonly;
+              if (c.readonly === true) {
+                b.valuedata = '';// 将配置为不可编辑的值置空
+              } else if (b.valuedata) {
+                if (b.fkdisplay === 'drp' || b.fkdisplay === 'mrp' || b.fkdisplay === 'pop' || b.fkdisplay === 'pop') { // 外键类型要特殊整合
+                  copySaveDataForParam[b.colname] = [{ ID: b.refobjid, Label: b.valuedata }];
+                } else if (b.fkdisplay === 'mop') {
+                  const number = JSON.parse(b.valuedata).lists.result.length;
+                  copySaveDataForParam[b.colname] = [{ ID: b.valuedata, Label: `已经选中${number}条数据` }];
+                } else {
+                  copySaveDataForParam[b.colname] = b.valuedata;// 重组数据添加到add
+                }
+              }
+            }
+          });
+        });
+      });
+    });
+    // Object.keys(modifyData).map((modify) => {//将修改过的值重新赋值给form所需数据
+    //   copyDatas.data.addcolums.forEach((copy) => {
+    //     copy.childs.forEach((childs) => {
+    //       if (modify === childs.colname) {
+    //         childs.valuedata = modifyData[modify];
+    //       }
+    //     });
+    //   });
+    // });
+    
+    state.updateData[tableName].changeData = Object.assign({}, copySaveDataForParam, modifyData);// 用于通过改变changeData触发form抛出值，以便保存时可以拿到add里面的值作为参数
+    state.tabPanels[0].componentAttribute.panelData.data = copyDatas.data;// 替换panelData新增逻辑接口返回数据，将上一界面值重新赋值给form
+    state.updateData = Object.assign({}, state.updateData);
+    state.tabPanels[0].componentAttribute.panelData.data = copyDatas.data;
+  },
+  emptyChangeData(state, tableName) {
+    state.updateData[tableName].changeData = {};
+  },
+  updateCopyDataForRealdOnly(state, data) {
+    state.copyDataForReadOnly = data;
+  },
+  updateButtonsExport(state, data) { // 导出
+    state.buttonsData.exportdata = data;
+  },
+  updatetooltipForItemTableData(state, data) { // 表格操作单条明细失败返回值
+    state.tooltipForItemTable = data;
+  },
+  updateSubmitData(state, submitData) { // 提交
+    state.buttonsData.submitData = submitData;
+  },
+  updateUnSubmitData(state, unSubmitData) { // 取消提交
+    state.buttonsData.unSubmitData = unSubmitData;
+  },
+  updateiInvalidData(state, invalidData) { // 作废
+    state.buttonsData.invalidData = invalidData;
+  },
+  updateObjTabActionSlientConfirm(state, data) { // 动作定义类型接口返回值
+    state.objTabActionSlientConfirmData = data;
+  },
+  updateLinkageForm(state, data) {
+    let mappStatus = {};
+    if (data.length > 0) {
+      mappStatus = data.reduce((arry, item) => {
+        if (item.srccol) {
+          arry[item.key] = item.srccol;
+        }
+        return arry;
+      }, {});
+      state.LinkageForm = state.LinkageForm.concat(data);
+      state.mappStatus = Object.assign(state.mappStatus, mappStatus);
+    } else {
+      state.LinkageForm = [];
+      state.mappStatus = {};
+    }
+  },
+  updateMaping(state, data) {
+    // form 联动校验map 图
+  //  获取映射关系
+    const temp = Object.keys(data).reduce((a, c) => {
+      const f = (key) => {
+        if (!data[key]) {
+          return [];
+        }
+        return [data[key]].concat(f(data[key]));
+      };
+      a[c] = f(c);
+      return a;
+    }, {});
+    return temp;
+  },
+  updateTableSearchData(state, data) {
+    const { tableSearchData } = state.tabPanels[state.tabCurrentIndex];
+    tableSearchData.selectedValue = data.selectedValue;
+    tableSearchData.inputValue = data.inputValue;
+  }, // 修改单对象表格搜索的值
+};
---
+++ src/__config__/mutations/version_1.4/standardTableList.mutations.js
@@ -0,0 +1,168 @@
+
+export default {
+  updateTableDataWithMerge({ ag }, data) {
+    ag.datas = Object.assign({}, ag.datas, data);
+  },
+  updateTableData({
+    ag
+  }, data) {
+    ag.datas = data;
+  },
+  updateTableStatus4css({
+    ag
+  }, data) {
+    ag.status4css = data;
+  },
+  // setAgTableErrorMessage({ ag }, errorData) { // ag iconfont
+  //   ag.datas.deleteFailInfo = Object.assign({}, ag.datas.deleteFailInfo, errorData);
+  //   console.log( ag.datas.deleteFailInfo )
+  // },
+
+  // 表单
+  updateDefaultFormItemsLists({ formItems }, data) {
+    formItems.defaultFormItemsLists = data;
+  },
+  updateFormData({
+    formItems
+  }, data) {
+    formItems.data = data;
+  },
+  updateDefaultSearchFoldnum({ formItems }, data) {
+    formItems.searchFoldnum = data;
+  },
+
+  // 按钮组
+  updateButtonsTabcmd({
+    buttons
+  }, data) {
+    buttons.tabcmd = data;
+  },
+  updateButtonWaListButtons({ buttons }, data) { // 获取静默类型的按钮数据
+    if (data) {
+      data.forEach((item, index) => {
+        if (item.ishide) {
+          data.splice(index);
+        }
+        buttons.isrefrsh = item.isrefrsh;
+      });
+    }
+   
+    buttons.dataArray.waListButtonsConfig.waListButtons = data;
+  },
+  collectTablelist({ buttons }) { // 判断页面加载时收藏按钮状态
+    const tableName = this.state.global.activeTab.tableName;
+    const favorite = this.state.global.favorite;
+    favorite.forEach((element) => {
+      if (element.value === tableName) {
+        buttons.dataArray.collectiImg = true;
+      }
+    }); 
+  },
+  updateDefaultButtonsdatas({
+    buttons
+  }, data) {
+    buttons.isBig = data.isbig; // 是否为海量表
+    buttons.isSolr = data.isolr; // 是否获取合计
+    buttons.showBigDefaultIcon = data.isbig;
+    buttons.tabledesc = buttons.tabcmd.desc;
+    buttons.tableId = data.tableid;
+    buttons.tableName = data.table;
+    // 双击条状判断
+    buttons.objTableUrl = data.tableurl;
+  },
+  setActiveTabActionValue({
+    buttons
+  }, obj) {
+    buttons.activeTabAction = obj;
+  },
+  updateButtonExeActionData({ buttons }, message) {
+    buttons.ExeActionData = message;
+  },
+  updateButtonGetActionData({ buttons }, data) {
+    buttons.ExeActionDataForComponent = data;
+  },
+  updateButtonSetFavoriteData({ buttons }, data) { // 收藏
+    if (data.code === 0) {
+      buttons.dataArray.collectiImg = !buttons.dataArray.collectiImg;
+      this.state.global.favorite = data.data;
+    }
+  },
+  updateDefaultButtonGroupData({ buttons }, data) {
+    buttons.dataArray.buttonGroupShowConfig.buttonGroupShow = data;
+  },
+  onSelectionChangedAssignment({ buttons }, { rowIdArray, rowArray }) {
+    buttons.selectIdArr = rowIdArray;
+    buttons.selectArr = rowArray;
+  },
+  setImportDialogTitle({ buttons }) { // 导出操作
+    buttons.importData.importDialog = true;
+    buttons.importData.importDialogTitle = this.state.global.activeTab.label;
+  },
+  closeImportDialog({ buttons }) { // 导出操作
+    buttons.importData.importDialog = false;
+  },
+  updateButtonImportGetUploadParameters({ buttons }, data) { // 获取导入上传参数
+    buttons.importParameters = data;
+  },
+  updateButtonDownloadImportTemplate({ buttons }, data) {
+    buttons.importTemplate = data;
+  },
+  updateButtonDeleteData({ buttons, ag }, data) {
+    buttons.batchDeleteData = data;
+    ag.datas.deleteFailInfo = data.data;
+    ag.datas = Object.assign({}, ag.datas);
+  },
+  setErrorModalValue({ buttons }, {
+    title, contentText, footerHide 
+  }) { // 设置error弹窗提示信息
+    buttons.dialogConfig = {
+      title,
+      footerHide,
+      contentText,
+    };
+  },
+  setActionDialog({ buttons }, obj) {
+    // buttons.actionDialog.queryString = obj.action.split('/')[1];
+    buttons.actionDialog.show = true;
+    buttons.actionDialog.title = obj.webdesc;
+  },
+  updateButtonbatchSubmitData({ buttons, ag }, data) { // 批量提交
+    buttons.batchSubmitData = data;
+    ag.datas.deleteFailInfo = data;
+    ag.datas = Object.assign({}, ag.datas);
+  },
+  updateButtonbatchUnSubmitData({ buttons, ag }, data) { // 批量反提交
+    buttons.batchUnSubmitData = data;
+    ag.datas.deleteFailInfo = data;
+    ag.datas = Object.assign({}, ag.datas);
+  },
+  batchVoidForButtonsData({ buttons, ag }, data) { // 作废
+    buttons.batchVoidForButtonsData = data;
+    ag.datas.deleteFailInfo = data;
+    ag.datas = Object.assign({}, ag.datas);
+  },
+  updateUserConfig(state, { userConfig }) {
+    // state.hideColumn = userConfig.hideColumn;
+    // state.colPosition = userConfig.colPosition;
+    // state.fixedColumn = userConfig.fixedColumn;
+    // 将控制隐藏列、列顺序、固定列等信息统一由state.userConfigForAgTable控制
+    state.userConfigForAgTable = {
+      hideColumn: userConfig.hideColumn,
+      colPosition: userConfig.colPosition,
+      fixedColumn: userConfig.fixedColumn
+    };
+  },
+  updateAgConfig(state, { key, value }) {
+    // e.g: state['hideColumn'] = [];
+    // e.g: state['colPosition'] = '';
+    // e.g: state['fixedColumn'] = '';
+    state.userConfigForAgTable = Object.assign({}, state.userConfigForAgTable, {
+      [key]: value
+    });
+  },
+  updateButtonsExport({ buttons }, data) { // 导出
+    buttons.exportdata = data;
+  },
+  
+ 
+};
