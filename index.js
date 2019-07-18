e8955bdd87e10a181d62f2d88b63d3d5d76735d1
mayun5767 <576747591@qq.com>
2019-07-18 09:54:23 +0800

    字体库打包 1

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ import CompositeForm from './src/__component__/CompositeForm';
 import customizedModalConfig from './src/__config__/customizeDialog.config';
 // css import
 import 'burgeon-ui/src/styles/common/iconfont/bjIconfonts/iconfont';
+import 'burgeon-ui/src/styles/common/iconfont/ionicons-font.less';
 import './node_modules/ag-grid/dist/styles/ag-grid.css';
 import './src/assets/r3.iconfont/iconfont.css';
-import 'burgeon-ui/dist/styles/fonts/iconfont';
-
 import './src/assets/css/ag-theme-balham.less';
 import './src/assets/theme/custom.less';
 import './src/assets/css/loading.css';
