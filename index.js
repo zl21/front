cc4217e7b57c55e3ffca2346319add76788c2663
mayun5767 <576747591@qq.com>
2019-07-18 09:36:10 +0800

    测试修改 字体库

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ import CompositeForm from './src/__component__/CompositeForm';
 import customizedModalConfig from './src/__config__/customizeDialog.config';
 // css import
 import 'burgeon-ui/src/styles/common/iconfont/bjIconfonts/iconfont';
-import 'burgeon-ui/dist/styles/fonts/iconfont';
 import './node_modules/ag-grid/dist/styles/ag-grid.css';
 import './src/assets/r3.iconfont/iconfont.css';
+import 'burgeon-ui/dist/styles/fonts/iconfont';
+
 import './src/assets/css/ag-theme-balham.less';
 import './src/assets/theme/custom.less';
 import './src/assets/css/loading.css';
