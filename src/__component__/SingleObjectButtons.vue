539a08c9dbd4943383f43a2a16791ebd97bdcda0
xia.hj <xia.hj@burgeon.cn>
2019-08-08 14:39:53 +0800

    解决单对象复制保存时报错

--- src/__component__/SingleObjectButtons.vue
+++ src/__component__/SingleObjectButtons.vue
@@ -770,7 +770,6 @@
       objectCopy() { // 按钮复制功能
         const id = 'New';// 修改路由,复制操作时路由为新增
         const label = `${this.activeTab.label.replace('编辑', '新增')}`;
-       
         if (this.objectType === 'horizontal') { // 横向布局
           if (this.tabCurrentIndex === 0) { // 主表
             let formData = {};
@@ -780,12 +779,9 @@
               }
             });
             const copyData = { ...formData };
-           
             const modifyData = this.updateData[this.tableName].changeData;// 取changeData值，因外键形式需要lable和ID
-
             this.copyDataForSingleObject({ copyData });// 将复制所保存的数据存到global中
             this.copyModifyDataForSingleObject(modifyData);// 将复制修改过所保存的数据存到global中
-
             this.updateFormDataForRefshow();
             const type = 'tableDetailHorizontal';
             this.tabHref({// 跳转路由，复制是新增逻辑
--- src/__config__/mutations/version_1.4/horizontalTableDetail.mutations.js
+++ src/__config__/mutations/version_1.4/horizontalTableDetail.mutations.js
@@ -162,7 +162,11 @@ export default {
             if (b.name === c.name) {
               b.readonly = c.readonly;
               if (c.readonly === true) {
-                b.valuedata = '';// 将配置为不可编辑的值置空
+                if (d.parentdesc === '日志' && b.display === 'check') {
+                  b.valuedata = 'N'; // check类型
+                } else {
+                  b.valuedata = '';// 将配置为不可编辑的值置空
+                }
               } else if (b.valuedata) {
                 if (b.fkdisplay === 'drp' || b.fkdisplay === 'mrp' || b.fkdisplay === 'pop' || b.fkdisplay === 'pop') { // 外键类型要特殊整合
                   copySaveDataForParam[b.colname] = [{ ID: b.refobjid, Label: b.valuedata }];
@@ -191,6 +195,8 @@ export default {
     state.updateData[tableName].changeData = Object.assign({}, copySaveDataForParam, modifyData);// 用于通过改变changeData触发form抛出值，以便保存时可以拿到add里面的值作为参数
     state.updateData = Object.assign({}, state.updateData);
     state.tabPanels[0].componentAttribute.panelData.data = copyDatas.data;// 替换panelData新增逻辑接口返回数据，将上一界面值重新赋值给form
+    console.log(1, state.tabPanels[0].componentAttribute.panelData.data);
+    console.log(2, copyDatas.data);
 
     // state.tabPanels[0].componentAttribute.panelData.data = copyDatas.data;
   },
