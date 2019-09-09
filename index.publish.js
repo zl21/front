e04705a3dbaab8392bd799c32ebdd3bcb6e8e5e2
xia.hj <xia.hj@burgeon.cn>
2019-09-09 14:43:21 +0800

    修改README

--- index.publish.js
+++ index.publish.js
@@ -55,6 +55,7 @@ export default {
    *   extentionForColumn: [], // 项目自定义“字段”扩展属性列表
    *   enableModuleKA: true, //  启用KeepAlive
    *   enableTabLists: true, // 启用TabLits模块页签切换
+   *   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用功能
    * }
    */
   launchApplication(projectConfig) {
