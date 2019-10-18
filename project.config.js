35aafc9837dcc9b352fe26f10d3aaf85a4325531
843748862@qq.com <843748862@qq.com>
2019-10-18 13:38:54 +0800

    关闭jflow插件

--- project.config.js
+++ project.config.js
@@ -4,9 +4,9 @@ module.exports = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
-  // target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
+  target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
   // target: 'http://r3dev.qiaodan.com:28889', // 乔丹开发环境
-  target: 'http://dev.burgeononline.com:8120', // 掌云测试环境(启用网关)
+  // target: 'http://dev.burgeononline.com:8120', // 掌云测试环境(启用网关)
   Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
   enableGateWay: true, // 网关是否打开,
@@ -14,7 +14,7 @@ module.exports = {
   ignoreGateWayPattern: [/\/jflow\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配。
   enableNetworkMonitor: true, // 是否开启网络监控功能
   enableModuleKA: true, // 默认启用keepAlive功能
-  enableJflow: true, // 默认禁用JFlow插件功能
+  enableJflow: false, // 默认禁用JFlow插件功能
   jflowRequestDomain: '123123', // jflow后台地址
   enableTabLists: true, // 是否启用Tab页签切换模块的能力，默认启用
   touristRoute: '/login', // 游客路由：等用户未登录的时候，需要跳转的页面
