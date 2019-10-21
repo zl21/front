9bc969231ee8ebb7ce151399abd5e19bbfaba00a
xia.hj <xia.hj@burgeon.cn>
2019-10-21 16:23:31 +0800

    测试

--- project.config.js
+++ project.config.js
@@ -4,10 +4,10 @@ module.exports = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
-  // target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
-  target: 'http://r3dev.qiaodan.com:28889', // 乔丹开发环境
+  target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
+  // target: 'http://r3dev.qiaodan.com:28889', // 乔丹开发环境
   // target: 'http://dev.burgeononline.com:8120', // 掌云测试环境(启用网关)
-  Version: '1.3', // 版本号
+  Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
   enableGateWay: false, // 网关是否打开,
   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用功能
