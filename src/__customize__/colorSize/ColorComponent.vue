8bc6e5bd183f875976bd2cbaabd4d4782a12495d
qubingnian <qu.bn@burgeon.cn>
2019-10-21 18:10:58 +0800

    添加颜色尺寸界面

--- src/__customize__/colorSize/ColorComponent.vue
+++ src/__customize__/colorSize/ColorComponent.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="color_container">
     <div class="left_container">
-        <div class="left_button">
-            <Button type="fcdefault">
-                列出所有颜色
-            </Button>
-        </div>
-        <div class="leftTable" id="leftTable" ref="leftTable">
-            <Table
-                    :columns="leftTableColumns"
-                    :height="leftTableHeight"
-                    :data="leftTableData"
-                    @on-row-click="leftTableRowClick"
-            />
-        </div>
+      <div class="left_button">
+        <Button
+          type="fcdefault"
+          @click="listAllColor"
+        >
+          列出所有颜色
+        </Button>
+      </div>
+      <div
+        ref="leftTable"
+        class="left_table"
+      >
+        <Table
+          :columns="leftTableColumns"
+          :height="leftTableHeight"
+          highlight-row
+          :data="leftTableData"
+          @on-row-click="leftTableRowClick"
+        />
+      </div>
     </div>
     <div class="center_container">
       <div class="right_single">
@@ -41,28 +48,97 @@
         />
       </div>
     </div>
-      <div class="right_container">
-          <Table
-                  :columns="rightTableColumns"
-                  class="rightTable"
-                  :data="rightTableData"
-                  @on-row-click="rightTableRowClick"
-          />
+    <div class="right_container">
+      <div class="right_input">
+        <Input
+          v-model="addColorInputValue"
+          placeholder="请输入颜色编码/颜色名称"
+          icon="ios-add-circle-outline"
+          @on-change="addColorInputChange"
+          @on-click="addIconClick"
+        >
+        <Button
+          slot="prepend"
+          @click="addColor"
+        >
+          手动新增颜色
+        </Button>
+        </Input>
       </div>
+      <div
+        ref="rightTable"
+        class="right_table"
+      >
+        <Table
+          :columns="rightTableColumns"
+          :data="rightTableData"
+          highlight-row
+          :height="rightTableHeight"
+          @on-row-click="rightTableRowClick"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import network, { urlSearchParams } from '../../__utils__/network';
+
   export default {
     data() {
       return {
         leftTableColumns: [
-          {title: 'xuhao', key: 'aaa'}
+          {
+            title: '序号',
+            type: 'index',
+            width: 60,
+            align: 'center'
+          },
+          {
+            title: '颜色编码',
+            key: 'ECODE'
+          },
+          {
+            title: '颜色名称',
+            key: 'ENAME'
+          }
         ], // 左边表格的表头
         leftTableData: [], // 左边表格数据
         leftTableHeight: 0, // 左边表格的高度
-        rightTableColumns: [], // 右边表格的表头
+        leftTableSelectIndex: null, // 左侧表格选中的下标
+
+        rightTableColumns: [
+          {
+            title: '序号',
+            type: 'index',
+            width: 60,
+            align: 'center'
+          },
+          {
+            title: '颜色编码',
+            key: 'ECODE'
+          },
+          {
+            title: '颜色名称',
+            key: 'ENAME'
+          },
+          {
+            title: '主副色',
+            render: this.mainColorRender()
+          },
+          {
+            title: '布料颜色',
+            render: this.fabColorRender()
+          },
+          {
+            title: '颜色图片',
+            key: 'image'
+          },
+        ], // 右边表格的表头
         rightTableData: [], // 右边表格数据
+        rightTableHeight: 0, // 右边表格的高度
+        addColorInputValue: '', // 手动新增颜色的输入框
+        rightTableSelectIndex: null, // 右侧的表格选中的下标
       };
     },
     name: 'ColorComponent',
@@ -70,27 +146,124 @@
     watch: {},
     computed: {},
     mounted() {
-      this.leftTableHeight = this.$refs.leftTable.offsetHeight - 31;
+      this.leftTableHeight = this.$refs.leftTable.offsetHeight;
+      this.rightTableHeight = this.$refs.rightTable.offsetHeight;
+    },
+    created() {
+      this.getRightTableData();
     },
     methods: {
       rightSingle() {
-
+        if (this.leftTableSelectIndex !== null) {
+          this.rightTableData.push(this.leftTableData[this.leftTableSelectIndex]);
+          this.leftTableData.splice(this.leftTableSelectIndex, 1);
+          this.leftTableSelectIndex = null;
+        }
       }, // 单个向右的icon点击触发
       rightDouble() {
-
+        this.rightTableData = this.rightTableData.concat(this.leftTableData);
+        this.leftTableData = [];
       }, // 两个向右的icon点击触发
       leftSingle() {
-
+        if (this.rightTableSelectIndex !== null) {
+          this.leftTableData.push(this.rightTableData[this.rightTableSelectIndex]);
+          this.rightTableData.splice(this.rightTableSelectIndex, 1);
+          this.rightTableSelectIndex = null;
+        }
       }, // 单个向左的icon点击触发
       leftDouble() {
-
+        this.leftTableData = this.leftTableData.concat(this.rightTableData);
+        this.rightTableData = [];
       }, // 两个向左的icon点击触发
-      leftTableRowClick() {
-
+      leftTableRowClick(val, index) {
+        this.leftTableSelectIndex = index;
       }, // 左边表格单选触发
-      rightTableRowClick() {
-
+      rightTableRowClick(val, index) {
+        this.rightTableSelectIndex = index;
       }, // 右边表格单选触发
+      listAllColor() {
+        const params = {
+          param: {
+            PS_C_PRO_ID: '22103',
+            FLAG: 1
+          }
+        };
+        network.get('/p/cs/cspecobjload', { params })
+          .then((res) => {
+            if (res.data.code === 0) {
+              this.leftTableData = res.data.data;
+            }
+          });
+      }, // 列出所有颜色按钮点击
+      addColor() {
+
+      }, // 手动新增颜色点击触发
+      addColorInputChange() {
+
+      }, // 手动新增颜色输入框改变时触发
+      addIconClick() {
+
+      }, // 手动新增颜色，新增icon点击时触发
+      getRightTableData() {
+        const params = {
+          param: {
+            PS_C_PRO_ID: '22103'
+          }
+        };
+        network.get('/p/cs/cprospecload', { params })
+          .then((res) => {
+            if (res.data.code === 0) {
+              this.rightTableData = res.data.data.COLOR;
+            }
+          });
+      }, // 获取右边表格的数据
+      mainColorRender() {
+        return (h, params) => h('Select', {
+          style: {
+          },
+          props: {
+            transfer: true,
+            clearable: true,
+            value: ''
+          },
+          nativeOn: {
+            click: (e) => {
+              e.stopPropagation();
+            }
+          },
+          on: {
+            'on-change': (event, data) => {
+            }
+          }
+        }, this.selectOptionRender(h));
+      }, // 右边表格主副色render
+      selectOptionRender(h) {
+        const colorType = [{ name: '主色', code: 1 }, { name: '副色', code: 2 }, { name: '配卖', code: 3 }];
+        return colorType.map(item => h('Option', {
+          props: {
+            value: item.code,
+            label: item.name
+          }
+        }));
+      },
+      fabColorRender() {
+        return (h, params) => h('Input', {
+          style: {
+          },
+          props: {
+            value: ''
+          },
+          nativeOn: {
+            click: (e) => {
+              e.stopPropagation();
+            }
+          },
+          on: {
+            'on-change': (event, data) => {
+            }
+          }
+        });
+      }, // 右边表格主副色render
     }
   };
 </script>
@@ -112,7 +285,7 @@
             .left_button {
                 margin-bottom: 8px;
             }
-            .leftTable {
+            .left_table {
                 flex: 1;
                 overflow: hidden;
             }
@@ -151,8 +324,32 @@
             border-radius: 6px;
             padding: 10px;
             overflow: auto;
-            .rightTable {
-                margin-top: 5px;
+            display: flex;
+            flex-direction: column;
+            .right_input {
+                width: 50%;
+                margin-bottom: 8px;
+                .burgeon-input-group {
+                    top: 0px;
+                }
+                .burgeon-input-group-prepend {
+                    .burgeon-btn {
+                        display: flex;
+                        align-items: center;
+                        span {
+                            position: relative;
+                        }
+                    }
+                }
+            }
+            .right_table {
+                flex: 1;
+                .burgeon-select-selection {
+                    height: 22px;
+                }
+                .burgeon-input-wrapper > input {
+                    height: 22px;
+                }
             }
         }
     }
