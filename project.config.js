f460222a452cc1a7c3ed3a8ee3998f687e5368eb
xia.hj <xia.hj@burgeon.cn>
2019-10-25 17:11:00 +0800

    处理userConfigForAgTable

--- project.config.js
+++ project.config.js
@@ -4,15 +4,18 @@ module.exports = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
-  target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
+  // target: 'http://47.99.229.124:8098', // 框架研发网关开启环境
   // target: 'http://r3dev.qiaodan.com:28889', // 乔丹开发环境
   // target: 'http://r3dev.qiaodan.com:26666', // 乔丹开发环境
   // target: 'http://dev.burgeononline.com:8120', // 掌云测试环境(启用网关)
   // target: 'http://dev.burgeononline.com:8098', // 掌云测试环境(启用网关)
   // target: 'http://r3.burgeon.cn:28888', // 李宁
-  Version: '1.4', // 版本号
+  target: 'http://139.224.169.76:28888', // 沁园
+
+  
+  Version: '1.3', // 版本号
   interlocks: true, // 是否打开三级联动装置
-  enableGateWay: true, // 网关是否打开,
+  enableGateWay: false, // 网关是否打开,
   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用功能
   ignoreGateWayPattern: [/\/jflow\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配。
   enableNetworkMonitor: true, // 是否开启网络监控功能
