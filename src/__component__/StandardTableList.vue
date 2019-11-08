fb7a3ac934c93438b02c708fb8b4d1d96e18c76b
xia.hj <xia.hj@burgeon.cn>
2019-11-08 18:05:31 +0800

    提交测试

--- src/__component__/StandardTableList.vue
+++ src/__component__/StandardTableList.vue
@@ -1682,10 +1682,13 @@
               linkId: tab.webid
             });
             const name = `${LINK_MODULE_COMPONENT_PREFIX}.${tab.webname.toUpperCase()}.${tab.webid}`;     
-            this.addKeepAliveLabelMaps({ name, label: tab.webdesc });
+            // this.addKeepAliveLabelMaps({ name, label: tab.webdesc });
             const linkUrl = tab.action;
             const linkId = tab.webid;
-            this.increaseLinkUrl({ linkId, linkUrl });
+            
+            if (!this.LinkUrl[linkId]) {
+              this.increaseLinkUrl({ linkId, linkUrl });
+            }
             const obj = {
               linkName: tab.webname,
               linkId: tab.webid,
