8203d0c6516e1f8562e7d19d920149710137b698
xia.hj <xia.hj@burgeon.cn>
2019-11-26 16:38:34 +0800

    增加导入输入框正则校验

--- src/__component__/C.TableKeepAlive.vue
+++ src/__component__/C.TableKeepAlive.vue
@@ -2,69 +2,39 @@
   <keep-alive
     :include="keepAliveLists"
   >
-    <component
-      :is="currentModule"
-    />
+    <component :is="currentTable" />
   </keep-alive>
 </template>
 
 <script>
   import Vue from 'vue';
-  import { mapState, mapActions, mapMutations } from 'vuex';
-  import PageNotFound from './PageNotFound';
-  import CustomizeModule from '../__config__/customize.config';
-  import { CUSTOMIZED_MODULE_PREFIX, CUSTOMIZED_MODULE_COMPONENT_PREFIX } from '../constants/global';
-  import mixins from '../__config__/mixins/customize';
+  import { mapState } from 'vuex';
+  import mixins from '../__config__/mixins/standardTableLists';
+  import { STANDARD_TABLE_LIST_PREFIX, STANDARD_TABLE_COMPONENT_PREFIX } from '../constants/global';
+  import StandardTableList from './StandardTableList';
   import moduleName from '../__utils__/getModuleName';
 
-
-  
-  const customizeModules = {};
-  Object.keys(CustomizeModule).forEach((key) => {
-    customizeModules[key.toUpperCase()] = CustomizeModule[key];
-  });
-  
   export default {
-    name: `${CUSTOMIZED_MODULE_COMPONENT_PREFIX}.Table.KeepAlive`,
+    name: `${STANDARD_TABLE_COMPONENT_PREFIX}.Table.KeepAlive`,
     data() {
       return {
-        currentModule: null
+        currentTable: null
       };
     },
     computed: {
       ...mapState('global', ['keepAliveLists'])
     },
     methods: {
-      ...mapActions('global', ['updateAccessHistory']),
-      ...mapMutations('global', ['addKeepAliveLabelMaps']),
-
       generateComponent() {
-        const externalModules = (window.ProjectConfig || { externalModules: undefined }).externalModules || {};
-        const { customizedModuleName, customizedModuleId } = this.$route.params;
         const { routePrefix } = this.$route.meta;
-        if (routePrefix !== CUSTOMIZED_MODULE_PREFIX) { return; }
-        const componentName = `${CUSTOMIZED_MODULE_COMPONENT_PREFIX}.${customizedModuleName}.${customizedModuleId}`;
+        if (routePrefix !== STANDARD_TABLE_LIST_PREFIX) { return; }
+        const componentName = moduleName();
         if (Vue.component(componentName) === undefined) {
-          const target = externalModules[customizedModuleName] || customizeModules[customizedModuleName];
-          if (target) {
-            if (typeof target.component === 'function') {
-              Vue.component(componentName, target.component);
-              Vue.component(componentName)().then((result) => {
-                Vue.component(componentName, Vue.extend(Object.assign({ mixins: [mixins()] }, result.default)));
-              });
-              this.currentModule = componentName;
-            } else {
-              Vue.component(componentName, Vue.extend(Object.assign({ mixins: [mixins()] }, target.component)));
-              this.currentModule = componentName;
-            }
-          } else {
-            Vue.component(componentName, PageNotFound);
-            this.currentModule = componentName;
-          }
-        } else {
-          this.currentModule = componentName;
+          Vue.component(componentName, Vue.extend(Object.assign({ mixins: [mixins()], isKeepAliveModel: true }, StandardTableList)));
         }
-      },
+     
+        this.currentTable = componentName;
+      } 
     },
     mounted() {
       this.generateComponent();
