b986e66d39fe82cd1c9b49ee56e876bd86ace129
xia.hj <xia.hj@burgeon.cn>
2019-12-03 11:14:28 +0800

    修改临时包版本号

--- index.publish.js
+++ index.publish.js
@@ -24,7 +24,7 @@ const validateConfig = config => ({
 
 export default {
   version: '1.7.1',
-  temporaryVersion: '1.7.2',
+  temporaryVersion: '1.7.3',
   /**
    * @param projectConfig 项目配置
    * projectConfig: {
