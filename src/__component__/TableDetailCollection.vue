f8f71c3abcdcef78a6b75f7909b87fb1ed820cdd
qubingnian <qu.bn@burgeon.cn>
2019-12-16 13:06:33 +0800

    单对象明细表格的校验兼容后端1.3版本

--- src/__component__/TableDetailCollection.vue
+++ src/__component__/TableDetailCollection.vue
@@ -3050,7 +3050,6 @@
         return null;
       },
       putDataFromCell(currentValue, oldValue, colname, IDValue, type, fkdisplay) {
-        console.log(currentValue, oldValue);
         // 组装数据 存入store
         if (!currentValue) {
           if (fkdisplay === 'mrp' || fkdisplay === 'mop') {
@@ -3446,7 +3445,7 @@
         data.map((ele) => {
           tabthData.forEach((col) => {
             if (col.isnotnull && col.colname !== EXCEPT_COLUMN_NAME && ele[col.colname] !== undefined) {
-              if (ele[col.colname] === '' || ele[col.colname] === 0) {
+              if (ele[col.colname] === '' || ele[col.colname] === 0 || ele[col.colname] === null) {
                 this.verifyTipObj[ele.ID] = `${col.name}不能为空，请输入！`;
               }
             }
