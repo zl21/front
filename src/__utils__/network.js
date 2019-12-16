af6f9feedb41da073401b4925d5a2286b80022cf
xia.hj <xia.hj@burgeon.cn>
2019-12-16 17:54:25 +0800

    删除log

--- src/__utils__/network.js
+++ src/__utils__/network.js
@@ -34,7 +34,6 @@ const matchGateWay = (url) => {
     return globalServiceId || undefined;
   }
   if (tableName) {
-    console.log(222,serviceIdMap)
     if (serviceIdMap[tableName] !== 'undefined') {
       const serviceIdMapApi = serviceIdMap[tableName];
       return serviceIdMapApi || undefined;
