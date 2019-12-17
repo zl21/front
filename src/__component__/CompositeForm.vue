8064052190d7e9502d06b59e4acbc6e4e598ac57
xia.hj <xia.hj@burgeon.cn>
2019-12-17 13:08:27 +0800

    增加单对象界面loading表单所有校验完成后关闭

--- src/__component__/CompositeForm.vue
+++ src/__component__/CompositeForm.vue
@@ -605,13 +605,13 @@
             if (this.defaultData.copy) {
               DispatchEvent('globalNoticeCopy', {
                 detail: {
-                  copy: true
+                  hideCopyLoading: true
                 }
               }); 
             } else {
               DispatchEvent('globaVerifyMessageClosed', {
                 detail: {
-                  loading: false
+                  hideLoadingForButton: true
                 }
               });
             }
