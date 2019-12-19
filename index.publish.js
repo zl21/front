eb59deba5c10b268a26a0f6df00f9a2eca06e5bc
xia.hj <xia.hj@burgeon.cn>
2019-12-19 10:05:39 +0800

    修改版本号

--- index.publish.js
+++ index.publish.js
@@ -24,7 +24,7 @@ const validateConfig = config => ({
 
 export default {
   version: '1.7.2',
-  temporaryVersion: '1.8.9',
+  temporaryVersion: '1.9.0',
   /**
    * @param projectConfig 项目配置
    * projectConfig: {
