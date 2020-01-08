6945d1b5efb8ceaa66c146b3e3644fb05a588331
qubingnian <qu.bn@burgeon.cn>
2020-01-08 14:57:14 +0800

    修改功能权限

--- src/__config__/customize.config.js
+++ src/__config__/customize.config.js
@@ -8,6 +8,7 @@
 //   },
 // };
 import functionPermission from '../__customize__/functionPower/FunctionPowerNew.vue';
+
 // import functionPermissionTest from '../__customize__/functionPower/FunctionPowerNew.vue';
 import proDesc from '../__customize__/pro_desc/pro_desc.vue';
 import standard from '../__customize__/colorSize/Standard.vue';
