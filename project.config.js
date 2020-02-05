9a3ae480fa03e7b72750c5f8b425f1f798988b27
843748862@qq.com <843748862@qq.com>
2020-02-05 14:33:57 +0800

    商飞开发环境

--- project.config.js
+++ project.config.js
@@ -25,7 +25,7 @@ module.exports = {
   enableJflow: true, // 默认禁用JFlow插件功能
   closeJflowIcon: false, // 控制是否是tab显示，既是否显示顶部的jflow图标
   encryptionJflow: false, // 控制jflow传参是否加密
-  jflowRequestDomain: 'http://jflow-zhixiao.dev.burgeononline.com', // jflow后台地址(知晓)
+  jflowRequestDomain: 'http://jflow-shangfei.dev.burgeononline.com', // jflow后台地址(知晓)
   // jflowRequestDomain: 'http://jflow-qiaodan.dev.burgeononline.com', // jflow后台地址（乔丹）
   // jflowRequestDomain: 'http://106.15.24.156:32940', // jflow后台地址(李宁)
   // jflowRequestDomain: 'http://10.14.5.198:8080', // jflow后台地址(商飞)
