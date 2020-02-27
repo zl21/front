c71ab39c123d23619de55e5193e0bd5b12e2520b
bi.j <bi.j@burgeon.cn>
2020-02-27 15:55:32 +0800

    请求问题

--- src/__customize__/taskList/taskList.vue
+++ src/__customize__/taskList/taskList.vue
@@ -15,13 +15,14 @@
         重置
       </Button>
     </div>
-    <FormItemComponent
-      ref="FormItemComponent"
-      :search-foldnum="7"
-      :form-item-lists="formItemsLists"
-      :form-items-data="formItems"
-      @formDataChange="formDataChange"
-    />
+    <div class="form-area">
+      <FormLayout
+        ref="FormItemComponent"
+        :default-column="4"
+        :defaultconfig="formItemsLists"
+        @formDataChange="formDataChange"
+      />
+    </div>
     <div class="table-area">
       <Page
         ref="page"
@@ -49,51 +50,45 @@
   </div>
 </template>
 <script>
-  import FormItemComponent from '../../__component__/FormItemComponent';
-  import ItemComponent from '../../__component__/ItemComponent.vue';
+  import network, { urlSearchParams } from '../../__utils__/network';
 
   export default {
-    components: {
-      FormItemComponent
-    },
     data() {
       return {
         searchData: {
-          table: '',
+          table: 'TASK_RECORD',
           startIndex: 0,
           range: 10,
-          orderby: undefined
+          fixedcolumns: { ISACTIVE: ['=Y'] },
+          column_include_uicontroller: true
         }, // 查询条件
         formItemsLists: [
           {
-            row: 1,
-            col: 1,
-            component: ItemComponent,
-            labelWidth: 90,
+            show: true, // 是否显示隐藏
+            row: 1, // 行高
+            col: 1, // 列宽
             item: {
               type: 'input',
-              title: '姓名/员工编号',
+              label: '姓名/员工编号',
               filed: 'EMPLOYEE',
               props: {
-                // regx: /^[0-9]*$/
+                placeholder: '请输入'
               },
               event: {
-                keydown: (event) => {
-               
-                }
+                keydown: (event) => {}
               }
             }
           },
           {
-            row: 1,
-            col: 1,
-            component: ItemComponent,
-            labelWidth: 90,
+            show: true, // 是否显示隐藏
+            row: 1, // 行高
+            col: 1, // 列宽
             item: {
               type: 'input',
-              title: '角色',
+              label: '角色',
               filed: 'instanceId',
               props: {
+                placeholder: '请输入'
               },
               event: {
                 keydown: (event) => {
@@ -105,33 +100,29 @@
             }
           },
           {
-            row: 1,
-            col: 1,
-            component: ItemComponent,
-            labelWidth: 90,
+            show: true, // 是否显示隐藏
+            row: 1, // 行高
+            col: 1, // 列宽
             item: {
               type: 'DatePicker',
-              title: '时间范围',
+              label: '时间范围',
               filed: 'instanceId',
               props: {
                 type: 'daterange'
               },
-              event: {
-              
-              }
+              event: {}
             }
           },
           {
-            row: 1,
-            col: 1,
-            component: ItemComponent,
-            labelWidth: 90,
+            show: true, // 是否显示隐藏
+            row: 1, // 行高
+            col: 1, // 列宽
             item: {
               type: 'select',
-              title: '任务来源',
+              label: '任务来源',
               filed: 'TABLE_NAME',
               props: {
-                //   regx:/^[0-9]*$/
+              //   regx:/^[0-9]*$/
               },
               event: {
                 keydown: (event) => {
@@ -141,20 +132,17 @@
                 }
               }
             }
-          },
+          }
         ],
         formItems: {
           EMPLOYEE: '',
           instanceId: '',
           BEGIN_TIME: '',
           TABLE_NAME: ''
-
         },
         columns: [], // 表头
         tableData: [], // 表体数据
-        pageAttribute: {
-
-        }
+        pageAttribute: {}
       };
     },
     computed: {
@@ -172,64 +160,72 @@
         }
       },
       resetForm() {
-        // 列表查询重置
-        // this.resetType = true;
-        // const promise = new Promise((resolve, reject) => {
-        //   const searchData = this.searchData;
-        // // this.getTableQueryForForm({ searchData, resolve, reject });
-        // });
+      // 列表查询重置
+      // this.resetType = true;
+      // const promise = new Promise((resolve, reject) => {
+      //   const searchData = this.searchData;
+      // // this.getTableQueryForForm({ searchData, resolve, reject });
+      // });
       },
+      getTableQuery() {
+        network.post('/p/cs/getTableQuery', urlSearchParams({
+          table: 'TASK_RECORD',
+          getcmd: 'y'
+        })).then((res) => {
+          if (res.data.code === 0) {
+            const data = res.data.data.datas;
+            console.log(data, '配置数据');
+            // this.formItemsLists=data.dataarray.map(item=>{
+
+            // })
+          }
+        });
+      }, // 获取查询条件的一些数据
       searchClickData(value) {
       // 按钮查找 查询第一页数据
-        //   if (!value) {
-        //     // 返回时查询之前页码
-        //     this.searchData.startIndex = 0;
-        //   }
-        //   this.searchData.fixedcolumns = this.dataProcessing();
-        //   this.getQueryListForAg(this.searchData);
-        //   this.onSelectionChangedAssignment({ rowIdArray: [], rowArray: [] }); // 查询成功后清除表格选中项
+      //   if (!value) {
+      //     // 返回时查询之前页码
+      //     this.searchData.startIndex = 0;
+      //   }
+      //   this.searchData.fixedcolumns = this.dataProcessing();
+      //   this.getQueryListForAg(this.searchData);
+      //   this.onSelectionChangedAssignment({ rowIdArray: [], rowArray: [] }); // 查询成功后清除表格选中项
       },
-      formDataChange(data, item, index) { // 表单数据修改
+      formDataChange(data, item, index) {
+        // 表单数据修改
         if (JSON.stringify(this.formItems) !== JSON.stringify(data)) {
           if (this.formItemsLists.length > 0) {
             this.formItemsLists[index].item.value = item.item.value;
           }
 
-          // this.updateFormData(data);
+        // this.updateFormData(data);
         }
       },
-      pageChange(val) {
-
-      }, // 页码变化
+      pageChange(val) {}, // 页码变化
       pageSizeChange(val) {
         console.log(val);
       }, // 每页显示条数变化
       rowClassName(row) {
         console.log(row, '行数据');
       },
-      tableSelectionChange() {
-
-      },
-      tableRowDbclick() {
-
-      },
-      tableRowClick() {
-
-      },
-      tableSortChange() {
-
-      }
-    
+      tableSelectionChange() {},
+      tableRowDbclick() {},
+      tableRowClick() {},
+      tableSortChange() {}
+    },
+    created() {
+      this.getTableQuery();
     }
+
   };
 </script>  
 <style lang="less">
 .task-list-container {
-  .btn-area{
-     padding: 8px 0px;
-      button {
+  .btn-area {
+    padding: 8px 0px;
+    button {
       height: 22px;
-      font-size:12px;
+      font-size: 12px;
       line-height: 18px;
       margin-right: 8px;
       margin-left: 0px;
@@ -238,6 +234,35 @@
     }
     > button:last-child {
       margin-right: 0px;
+    }
+  }
+  .form-area {
+    padding: 8px 26px 8px 0px;
+    border: 1px solid #d8d8d8;
+    .formlayout-Item {
+      box-sizing:border-box;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-top: 8px;
+      .formlayout-div{
+        padding-left:8px;
+        height:26px;
+        border: 1px solid #d8d8d8;
+        overflow: hidden;
+        input{
+          width:100%;
+          height:100%;
+          border: none;
+        }
+        select{
+          width:100%;
+          height:100%;
+          border: none;
+        }
+      }
     }
   }
 }
