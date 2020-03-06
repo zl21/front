9ccb40632fb58a34fff1d7c5d5031424792fce2d
xia.hj <xia.hj@burgeon.cn>
2020-03-06 16:39:35 +0800

    修改配置

--- project.config.js
+++ project.config.js
@@ -4,13 +4,13 @@ module.exports = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
-  // target: 'http://r3-8098.dev.burgeononline.com/', // 框架研发网关开启环境
+  target: 'http://r3-8098.dev.burgeononline.com/', // 框架研发网关开启环境
   // target: 'http://r3dev.qiaodan.com:28889', // 乔丹开发环境
   // // target: 'http://r3dev.qiaodan.com:26666', // 乔丹开发环境
   // target: 'http://jflow-shangfei-r3.dev.burgeononline.com', // 知晓测试环境(启用网关)
   // target: 'http://zhixiao.burgeononline.com:32769/', // 知晓正式环境(启用网关)
   // target: 'http://dev.burgeononline.com:8098', // 掌云测试环境(启用网关)
-  target: 'http://r3.burgeon.cn:28888', // 李宁
+  // target: 'http://r3.burgeon.cn:28888', // 李宁
   // target: 'http://106.15.24.156:19093', // 李宁AD
   // target: 'http://139.224.169.76:28888', // 沁园
   // target: 'http://10.14.5.196', // 商飞开发环境
@@ -19,9 +19,9 @@ module.exports = {
   // target: 'http://r3-ishop.dev.burgeononline.com/', // ishop网关开启环境
 
   
-  Version: '1.3', // 版本号
+  Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
-  enableGateWay: false, // 网关是否打开,
+  enableGateWay: true, // 网关是否打开,
   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用功能
   ignoreGateWayPattern: [/\/jflow\/*/g, /\/api\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配
   enableNetworkMonitor: true, // 是否开启网络监控功能
