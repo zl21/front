cd014fbd25a746de7ffb1df79a925ea7a2ebaad2
xia.hj <xia.hj@burgeon.cn>
2020-03-06 16:22:32 +0800

    处理hr外面不可编辑字段的默认值逻辑

--- src/__config__/mutations/version_1.4/verticalTableDetail.mutations.js
+++ src/__config__/mutations/version_1.4/verticalTableDetail.mutations.js
@@ -393,8 +393,46 @@ export default {
                   }
                 }
               });
-            }
+            } 
           });
+        } else if (!item.childs) { // 处理hr外面不可编辑字段的默认值逻辑
+          const b = item.child;
+          const c = d.child;
+          if (b.name === c.name) {
+            b.readonly = c.readonly;
+            if (c.readonly === true) {
+              if (c.defval) { // 处理复制时有不可编辑，且有默认值情况
+                if (JSON.stringify(modifyData) !== '{}') { // 修改新增时不可编辑且有默认值，将修改后的值删除
+                  delete (modifyData[b.colname]);
+                }
+                if (c.display === 'select' || c.display === 'check') {
+                  copySaveDataForParam[b.colname] = c.defval;
+                } else if (c.fkdisplay === 'drp' || c.fkdisplay === 'mrp' || c.fkdisplay === 'pop' || c.fkdisplay === 'mop') {
+                  copySaveDataForParam[b.colname] = [{ ID: c.refobjid, Label: c.defval }];
+                }
+              } else {
+                b.valuedata = '';// 将配置为不可编辑的值置空
+                if (b.fkdisplay === 'drp' || b.fkdisplay === 'mrp' || b.fkdisplay === 'pop' || b.fkdisplay === 'mop') {
+                  b.refobjid = '';
+                }
+              }
+            } else if (b.valuedata) {
+              if (b.display === 'doc') {
+                copySaveDataForParam[b.colname] = b.valuedata;
+              } else if (b.fkdisplay === 'drp' || b.fkdisplay === 'mrp' || b.fkdisplay === 'pop') {
+                copySaveDataForParam[b.colname] = [{ ID: b.refobjid, Label: b.valuedata }];
+              } else if (b.fkdisplay === 'mop') {
+                try {
+                  const number = JSON.parse(b.valuedata).lists.result.length;
+                  copySaveDataForParam[b.colname] = [{ ID: b.valuedata, Label: `已经选中${number}条数据` }];
+                } catch (e) {
+                  copySaveDataForParam[b.colname] = b.valuedata;
+                }
+              } else {
+                copySaveDataForParam[b.colname] = b.valuedata;
+              }
+            }
+          }
         }
       });
     });
