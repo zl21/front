b9ab574205179cf8de64d76964c9e1ed8b7118df
843748862@qq.com <843748862@qq.com>
2020-03-25 13:06:02 +0800

    jflow按钮的前置保存事件处理

--- src/plugin/button.js
+++ src/plugin/button.js
@@ -62,11 +62,14 @@ function buttonsResponse(e) {
     })
       .then(() => {
         // 处理前置事件保存之后再处理当前事件
+        console.log(beforeClickFunction);
         if (Object.keys(beforeClickFunction).length > 0) {
           buttonsResponse(beforeClickFunction);
           beforeClickFunction = {};
         }
       });
+
+    return;
   }
 
   if (e.detail.obj.button === 'fresh') {
@@ -114,7 +117,6 @@ function buttonsResponse(e) {
 
 // 按钮点击逻辑处理
 function clickFunction(e) {
-  beforeClickFunction = {};
   if (e.detail.obj.isSave) { // 按钮存在保存前置事件时
     beforeClickFunction = e;
     DispatchEvent('jflowClick', {
