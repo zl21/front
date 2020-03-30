494ff0714787ae5b8b7ebd701554076bc275581d
843748862@qq.com <843748862@qq.com>
2020-03-30 13:54:38 +0800

    处理css主题

--- src/__config__/plugin.config.js
+++ src/__config__/plugin.config.js
@@ -7,14 +7,6 @@ export default {
       '../__plugin__/NetworkMonitor/NetworkMonitor.vue'
     )
   },
-  INSTANCEMANAGEMENTLIST: {
-    name: '待办列表', // 菜单Tab显示的label
-    component: () => import(
-      /* webpackChunkName: 'NetworkMonitor' */
-      /* webpackMode: 'eager' */
-      '../__plugin__/InstanceManagementList/InstanceManagementList.vue'
-    )
-  },
   PRINTTEMPLATE: { 
     name: '设置模版', // 菜单Tab显示的label
     component: () => import(
