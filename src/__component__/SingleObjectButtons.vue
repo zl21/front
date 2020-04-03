8a3b2277fd2f820dccda42100e2c63b5992d5412
xia.hj <xia.hj@burgeon.cn>
2020-04-03 14:09:22 +0800

    log

--- src/__component__/SingleObjectButtons.vue
+++ src/__component__/SingleObjectButtons.vue
@@ -2973,7 +2973,6 @@
         }
       },
       clearEditData() {
-        console.log(1);
         if (this.objectType === 'vertical') {
           if (this.subtables() && this.itemNameGroup.length > 0) {
             this.clearMainEditData();
@@ -2988,8 +2987,6 @@
         }
       },
       clearMainEditData() {
-        console.log(1);
-
         this.updateChangeData({ tableName: this.tableName, value: {} });
         this.updateModifyData({ tableName: this.tableName, value: {} });
         this.updateAddDefaultData({ tableName: this.tableName, value: {} });
@@ -2997,8 +2994,6 @@
         this.updateDeleteData({ tableName: this.tableName, value: {} });
       },
       clearItemEditData() {
-        console.log(1);
-
         const itemNames = this.itemNameGroup.map((c) => {
           if (c.tableName !== this.tableName) {
             return c.tableName;
