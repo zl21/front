818fe25845b2d5cd16b72da35b361d33d12a739f
xia.hj <xia.hj@burgeon.cn>
2020-05-30 15:07:58 +0800

    修改包信息

--- index.publish.js
+++ index.publish.js
@@ -29,8 +29,8 @@ const validateConfig = config => ({
 export default {
   version: '1.8.3',
   temporaryVersion: '2.1.2',
-  packageTime: '2020.05.30.09.25', 
-  user: 'ishop(正式包)',                     
+  packageTime: '2020.05.30.15.07', 
+  user: 'payerpqa(正式包)',                     
   /**
    * @param projectConfig 项目配置
    * projectConfig: {
