0470fb7f1f951c0c6d93e2cc06f100fbc373803c
843748862@qq.com <843748862@qq.com>
2020-07-09 16:30:50 +0800

    字表字段排序

--- src/__component__/TableDetailCollection.vue
+++ src/__component__/TableDetailCollection.vue
@@ -1392,6 +1392,7 @@
         if (!data) {
           return [];
         }
+
         // 整合表头数据
         const columns = data
           .filter(ele => ele.name !== EXCEPT_COLUMN_NAME)
@@ -1407,20 +1408,15 @@
               param.renderHeader = this.tooltipRenderHeader();
             }
 
-            // 默认排序处理
-            const order = this.currentOrderList && this.currentOrderList.length > 0 ? this.currentOrderList[0] : null;
-            if (order && ele.name === order.column) {
-              param.sortType = order.asc ? 'asc' : 'desc';
-            }
             // warning 2019/06/17注释 数据后端已经排序好了 但是 ！！！ 点击后排序  刷新列表 默认展示的排序的图标颜色显示也会丢失
-            // if (this.dataSource.ordids && this.dataSource.ordids.length > 0) {
-            //   this.dataSource.ordids.map((order) => {
-            //     if (ele.colname === order.colname && param.title !== '序号') {
-            //       param.sortType = order.ordasc ? 'asc' : 'desc';
-            //     }
-            //     return order;
-            //   });
-            // }
+            if (this.dataSource.ordids && this.dataSource.ordids.length > 0) {
+              this.dataSource.ordids.map((order) => {
+                if (ele.colname === order.colname && param.title !== '序号') {
+                  param.sortType = order.ordasc ? 'asc' : 'desc';
+                }
+                return order;
+              });
+            }
             const item = Object.assign(ele, param);
             return item;
           });
