dc8d8ec600467608d04228b40dfa8e4d8f966af6
843748862@qq.com <843748862@qq.com>
2020-08-04 17:32:23 +0800

    配置修改

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ import jflowPlugin from './jflowPlugin/js/index';
 
 Vue.use(jflowPlugin, {
   changePattern: true, // 控制待办列表转派的选择模式 true为单选,false为多选
+  gateway: 'ad-app'
 });
 
 // Vue.use(VueDND);
