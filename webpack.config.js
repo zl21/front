9b600a4f688195a3c16d4e127b1d4dbf0ab53536
xia.hj <xia.hj@burgeon.cn>
2020-08-13 11:23:00 +0800

    修复清除jflow回调文推

--- webpack.config.js
+++ webpack.config.js
@@ -105,9 +105,9 @@ module.exports = env => ({
         // target: 'http://106.15.24.156:32940', // 李宁环境
         // target: 'http://jflow-shangfei-yf.dev.burgeononline.com/', // 商飞环境
         // target: 'http://jflow-shangfei.dev.burgeononline.com/', // 新版jflow
-        target: 'http://jflow-dev-1-4.dev.burgeononline.com', // 商飞环境
+        // target: 'http://jflow-dev-1-4.dev.burgeononline.com', // 商飞环境
         // target: ' http://jflow-syman-dev.dev.burgeononline.com/', // 商飞测试环境
-        // target: ' http://47.102.164.111:26665/', // 卡宾环境
+        target: ' http://47.102.164.111:26666/', // 卡宾环境
         // target: ' http://zhixiao-jflow.pro.burgeononline.com/', // 知晓环境
         // target: 'http://jflow-qiaodan.dev.burgeononline.com', // 乔丹测试环境
         changeOrigin: true
