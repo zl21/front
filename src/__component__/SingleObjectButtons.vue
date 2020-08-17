709ecc0f6a469485a29caeabdf2a2f8ef1c1cc4b
xia.hj <xia.hj@burgeon.cn>
2020-08-17 11:41:04 +0800

    处理表单配置联动点击保存时校验出错问题

--- src/__component__/SingleObjectButtons.vue
+++ src/__component__/SingleObjectButtons.vue
@@ -2911,13 +2911,16 @@
         //     }
         //   });
         // }
-        if (this.itemId === 'New') { // 主表新增保存和编辑新增保存
-          if (this.verifyRequiredInformation()) { // 校验必填项
-            this.mainTableNewSaveAndEditorNewSave();
+        // 因表单失去焦点触发联动校验需一定的时间处理，故校验逻辑需要延时，因延时是异步的，无法return值，所以在调用保存时，处理延时逻辑
+        setTimeout(() => {
+          if (this.itemId === 'New') { // 主表新增保存和编辑新增保存
+            if (this.verifyRequiredInformation()) { // 校验必填项
+              this.mainTableNewSaveAndEditorNewSave();
+            }
+          } else if (this.itemId !== '-1') { // 主表编辑保存
+            this.mainTableEditorSave(obj);
           }
-        } else if (this.itemId !== '-1') { // 主表编辑保存
-          this.mainTableEditorSave(obj);
-        }
+        }, 600);
       },
       mainTableNewSaveAndEditorNewSave() { // 新增保存
         this.saveParameters();// 调用获取参数方法
