9f48f3cac7597720206ce887b0224e7567b5174a
843748862@qq.com <843748862@qq.com>
2020-09-21 14:07:11 +0800

    弹窗多选清除功能接口处理

--- src/__component__/ComplexsDialog.vue
+++ src/__component__/ComplexsDialog.vue
@@ -811,7 +811,7 @@
         this.sendMessage.PAGESIZE = 50;
         
         this.treeChecked();
-        this.multipleSelectionTable(this.sendMessage, 0);
+        // this.multipleSelectionTable(this.sendMessage, 0);
       },
       multipleSetMultiQuery(obj) {
         multipleComple().multipleSetMultiQuery({
