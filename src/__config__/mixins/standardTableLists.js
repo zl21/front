0f87c93a9c9cd8ab21782adfffd7871d974cd85b
xia.hj <xia.hj@burgeon.cn>
2020-10-14 14:23:48 +0800

    isCommonTable

--- src/__config__/mixins/standardTableLists.js
+++ src/__config__/mixins/standardTableLists.js
@@ -1,7 +1,7 @@
 import { mapState, mapActions, mapMutations } from 'vuex';
 import router from '../router.config';
 import {
-  STANDARD_TABLE_COMPONENT_PREFIX, STANDARD_COMMONTABLE_COMPONENT_PREFIX, MODULE_COMPONENT_NAME, INSTANCE_ROUTE, INSTANCE_ROUTE_QUERY, customizeMixins
+  isCommonTable, STANDARD_TABLE_COMPONENT_PREFIX, STANDARD_COMMONTABLE_COMPONENT_PREFIX, MODULE_COMPONENT_NAME, INSTANCE_ROUTE, INSTANCE_ROUTE_QUERY, customizeMixins
 } from '../../constants/global';
 import store from '../store.config';
 
