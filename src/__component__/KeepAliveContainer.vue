e1c0f538a2f1f454172130bace9a669d84631dce
xia.hj <xia.hj@burgeon.cn>
2020-10-21 14:23:54 +0800

    处理rpt类型逻辑

--- src/__component__/KeepAliveContainer.vue
+++ src/__component__/KeepAliveContainer.vue
@@ -127,7 +127,7 @@
           if (query.type === 'rpt') { // rpt类型特殊处理 
             // 元数据当前表type配置为rpt时，按照自定义界面逻辑执行路由逻辑，与自定义界面区别是，不再按照url内配置的"CUSTOMIZE/"后的自定义标示来加载自定义界面配置文件中的对应字段，
             // 而是根据路由的参数判断为rpt类型，则加载固定前端配置文件内的字段customizeReport字段对应的组件
-            target = externalModules.customizeReport || customizeModules.customizeReport;
+            target = externalModules.CUSTOMIZEREPORT || customizeModules.CUSTOMIZEREPORT;
           } else {
             target = externalModules[customizedModuleName] || customizeModules[customizedModuleName];
           }
