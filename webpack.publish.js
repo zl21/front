b3b32ce3251bbaf49f21566b2f0668185c5bf58c
xia.hj <xia.hj@burgeon.cn>
2020-10-29 09:40:18 +0800

    merge

--- webpack.publish.js
+++ webpack.publish.js
@@ -21,7 +21,7 @@ module.exports = () => ({
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
   externals: {
     vue: {
       commonjs: 'vue',
