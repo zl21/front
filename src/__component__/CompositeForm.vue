8b22c2fe2b3f9ba1d31a67a6f672c362b9cdc989
843748862@qq.com <843748862@qq.com>
2020-11-02 11:08:20 +0800

    字段联动传参修改

--- src/__component__/CompositeForm.vue
+++ src/__component__/CompositeForm.vue
@@ -1075,10 +1075,8 @@
                 }
 
                 await network.post(current.webconf.refcolval_custom.url, {
-                  data: {
-                    fixedcolumns: {
-                      [current.webconf.refcolval_custom.srccols]: check[1]
-                    }
+                  fixedcolumns: {
+                    [current.webconf.refcolval_custom.srccols]: check[1]
                   }
                 }).then((res) => {
                   if (res.data.code === 0) {
