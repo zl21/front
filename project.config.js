b5644787a98ad5213835ff6975e3f8c09f5465b8
xia.hj <xia.hj@burgeon.cn>
2020-11-30 16:23:21 +0800

    自定义弹框支持列表界面报错在对应明细显示报错信息

--- project.config.js
+++ project.config.js
@@ -32,6 +32,8 @@ module.exports = {
   // target: 'http://47.103.175.111:26666/', // 千百度环境
   // target: 'http://syman-jflow-standard.dev.burgeononline.com',
   // target: 'http://139.196.221.193:38888/', // 巧厨环境
+  // target: 'http://47.103.6.45:27777/', // 森马测试环境
+  
   
   Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
