a6959e78bf7df9fed88c80055c783f8cb146cd64
chenzihao <278199196@qq.com>
2020-12-21 17:45:18 +0800

    feat: 导航菜单label支持缩进(把本地配置注释掉)

--- src/__config__/navMenu.config.js
+++ src/__config__/navMenu.config.js
@@ -1,10 +1,10 @@
 // 配置菜单名称缩进
 
 export default {
-  PS_C_PRICE_STRATEGY: {
-    indent: 2
-  },
-  PS_C_PRICE_ADJUST: {
-    indent: 4
-  }
+  // PS_C_PRICE_STRATEGY: {
+  //   indent: 2
+  // },
+  // PS_C_PRICE_ADJUST: {
+  //   indent: 4
+  // }
 };
