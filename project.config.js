076af340831a8577d1300b3167fd86d14bf7be86
chenzihao <278199196@qq.com>
2021-01-07 09:44:12 +0800

    修改配置

--- project.config.js
+++ project.config.js
@@ -7,7 +7,7 @@ module.exports = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
-  // target: 'http://r3-8098.dev.syman.cn/', // 框架研发网关开启环境
+  target: 'http://r3-8098.dev.syman.cn/', // 框架研发网关开启环境
   // target: 'http://syman-jflow-standard.dev.burgeononline.com/', // 新版本jflow测试环境
   // target: 'http://r3dev.qiaodan.com:28888', // 乔丹开发环境
   // target: 'http://r3dev.qiaodan.com:26666', // 乔丹开发环境
@@ -36,11 +36,11 @@ module.exports = {
   // target: 'http://47.103.6.45:26666/', // 森马开发环境
   // target: 'http://r3dev.qiaodan.com:26666/', // 乔丹
   // target: 'http://oneretail-test.dev.syman.cn/', // 微淘氪
-  target: 'http://101.133.142.45:27777/', // 乔丹
+  // target: 'http://101.133.142.45:27777/', // 乔丹
   
-  Version: '1.3', // 版本号
+  Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
-  enableGateWay: false, // 网关是否打开,
+  enableGateWay: true, // 网关是否打开,
   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用接口请求
   enableHistoryAndFavoriteUI: true, // 是否显示收藏与最近使用UI
   ignoreGateWayPattern: [/\/jflow\/*/g, /\/api\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配
