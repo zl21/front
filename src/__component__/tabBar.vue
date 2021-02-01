afbaed52af648e0939e7a6202ba5653295cac731
chenzihao <278199196@qq.com>
2021-02-01 16:13:27 +0800

    Revert "update: 修改tab组件"
    This reverts commit 744c5ef4d1a31db24f4b2f6d568ed88175dc6a31.

--- src/__component__/tabBar.vue
+++ src/__component__/tabBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="r3-tab-nav">
-    <Tabs
-      type="card"
-      @on-click="tabClick"
+    <div
+      v-for="(item, index) in data.multi_tab"
+      :id="`tab_${index}`"
+      :key="index"
+      class="r3-tab-item"
+      :class="[index === currentIndex? 'r3-active-item' : '']"
+      @click="tabClick(index, item.tab_name,item.tab_value)"
     >
-      <TabPane
-        v-for="(item, index) in data.multi_tab"
-        :key="index"
-        :label="item.tab_name"
-      />
-    </Tabs>
+      {{ item.tab_name }}
+    </div>
   </div>
 </template>
 
@@ -19,7 +19,7 @@
     props: {
       data: {
         type: Object,
-        default: () => { }
+        default: () => {}
       },
 
     },
@@ -32,11 +32,18 @@
 
     methods: {
       tabClick(index) {
+        // const dom = document.querySelector('.r3-active-item');
+        // if (`tab_${index}` === dom.id) {
+        //   return;
+        // }
+        // if (this.lastIndex !== index) {
         this.currentIndex = index;
         this.$emit('tabClick', {
           data: this.data.multi_tab[index],
           index
         });
+        // this.lastIndex = index;
+        // }
       },
     },
   };
@@ -47,46 +54,22 @@
 .r3-tab-nav {
   display: flex;
   background: #f5f7f7;
+  padding: 8px 8px 0;
   margin-top: 20px;
 }
-</style>
-
-<style lang="less">
-@import url('../assets/theme/custom.less');
-.r3-tab-nav {
-  .ark-tabs-bar {
-    margin-bottom: 0;
-    border-bottom: none;
-    width: 100%;
-  }
-
-  .ark-tabs-card {
-    width: 100%;
-  }
-
-  .ark-tabs.ark-tabs-card > .ark-tabs-bar .ark-tabs-tab {
-      background: #f5f7f7;
-      color: @primary-color;
-      border: none;
-      line-height: 22px;
-      width: auto;
-      padding: 4px 20px;
-      font-weight: bold;
-      &:hover {
-        color: @primary-color;
-      }
-      &:last-child {
-        border-right: none;
-      }
-    }
-
-  .ark-tabs.ark-tabs-card > .ark-tabs-bar .ark-tabs-tab-active {
+  .r3-tab-item {
+    border-radius: 4px 4px 0 0;
+    font-size: 12px;
+    font-weight: bold;
+    color: @primary-color;
+    background:#f5f7f7;
+    padding: 0 14px;
+    height: 25px;
+    line-height: 25px;
+    cursor: pointer;
+    &.r3-active-item {
       background: @primary-color;
-      border-color: @primary-color;
       color: #f5f7f7;
-      &:hover {
-        color: #f5f7f7;
-      }
     }
-}
+  }
 </style>
