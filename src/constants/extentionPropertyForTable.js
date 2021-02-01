f9cccd7b596e24eb993b4ceb359d9a6b76771988
chenzihao <278199196@qq.com>
2021-02-01 13:35:03 +0800

    update: 表配置文案

--- src/constants/extentionPropertyForTable.js
+++ src/constants/extentionPropertyForTable.js
@@ -320,12 +320,12 @@ export default [
     description: '增加列表的查询tab页配置'
   },
   {
-    name: '关联字段组配置',
+    name: '单据标记配置',
     key: 'key_group_conf',
     type: 'custom',
     component: () => import('../__component__/ExtentionsProperty/bindKey.vue'),
     showLabel: false,
     enableIncrease: true,
-    description: '增加关联字段的配置'
+    description: '单据标记配置的配置'
   },
 ];
