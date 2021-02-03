a8890a31385ec5835b660570b7dcc69bd5fcb4d4
xia.hj <xia.hj@burgeon.cn>
2021-02-03 19:31:52 +0800

    merge

--- src/__config__/actions/version_1.4/standardTableList.actions.js
+++ src/__config__/actions/version_1.4/standardTableList.actions.js
@@ -147,37 +147,37 @@ export default {
           commit('updateDefaultFormItemsLists', queryData.datas.dataarry);
           commit('updateDefaultButtonsdatas', queryData.datas);
           commit('updateDefaultSearchFoldnum', queryData.datas.searchFoldnum);
-          queryData.datas.listbutton_filter_conf = [
-            {
-              action_id: '动作定义id1',
-              filter: [
-                {
-                  col_id: 'ISACTIVE',
-                  match_value: '是,否'
+          // queryData.datas.listbutton_filter_conf = [
+          //   {
+          //     action_id: '动作定义id1',
+          //     filter: [
+          //       {
+          //         col_id: 'ISACTIVE',
+          //         match_value: '是,否'
 
-                },
-                {
-                  col_id: 'BILLDATE',
-                  match_value: '2019-08-06,2019-08-08'
+          //       },
+          //       {
+          //         col_id: 'BILLDATE',
+          //         match_value: '2019-08-06,2019-08-08'
 
-                }
-              ]
-            },
-            {
-              action_id: '动作定义id2',
-              filter: [
-                {
-                  col_id: 'ISACTIVE',
-                  match_value: '是,否'
-                },
-                {
-                  col_id: 'BILLDATE',
-                  match_value: '2019-08-06,2019-08-08'
+          //       }
+          //     ]
+          //   },
+          //   {
+          //     action_id: '动作定义id2',
+          //     filter: [
+          //       {
+          //         col_id: 'ISACTIVE',
+          //         match_value: '是,否'
+          //       },
+          //       {
+          //         col_id: 'BILLDATE',
+          //         match_value: '2019-08-06,2019-08-08'
 
-                }
-              ]
-            }
-          ];
+          //       }
+          //     ]
+          //   }
+          // ];
 
           commit('updateFilterButtons', queryData.datas.listbutton_filter_conf);
           // queryData.datas.multi_tab = [
