90cef523f0ea432e7c65ee07e9e56b4e7c9c3690
xia.hj <xia.hj@burgeon.cn>
2021-02-07 17:02:53 +0800

    提取getProjectQuietRoutes方法

--- src/constants/global.js
+++ src/constants/global.js
@@ -125,3 +125,8 @@ export const navConfig = () => (window.ProjectConfig && window.ProjectConfig.nav
 export const notificationOfMain = () => (window.ProjectConfig && typeof window.ProjectConfig.notificationOfMain === 'boolean' ? window.ProjectConfig.notificationOfMain : project.notificationOfMain);
 export const formItemConfig = () => (window.ProjectConfig && window.ProjectConfig.formItemConfig ? window.ProjectConfig.formItemConfig : customizeFormItem);
 export const listDefaultColumn = () => (window.ProjectConfig && window.ProjectConfig.listDefaultColumn ? window.ProjectConfig.listDefaultColumn : 4); // 配置列表查询条件一行几列展示
+
+export const getProjectQuietRoutes = () => {
+  const { quietRoutes } = window.ProjectConfig || {};
+  return (defaultQuietRoutes.concat(quietRoutes || [])) || [];
+};
