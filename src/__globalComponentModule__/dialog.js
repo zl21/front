8b686d26b7e28d407bd93684b83d7ebf0af9b818
chenzihao <278199196@qq.com>
2021-03-04 18:34:19 +0800

    fix: 往dialog挂载自定义组件，报错组件未注册成功

--- src/__globalComponentModule__/dialog.js
+++ src/__globalComponentModule__/dialog.js
@@ -2,12 +2,20 @@ import DialogComponent from '../__component__/Dialog.vue';
 
 const Dialog = {
   install(Vue) {
-    const DialogConstructor = Vue.extend(DialogComponent);
-    const instance = new DialogConstructor();
-    instance.$mount(document.createElement('div'));
-    document.body.appendChild(instance.$el);
-
     Vue.prototype.$R3Dialog = function init(options, okFn, cancelFn) {
+      const domId = `r3-dialog-${options.dialogComponentName}`;
+      const contentDom = document.querySelector(`#${domId}`);
+      if (contentDom) {
+        contentDom.remove();
+      }
+
+      const DialogConstructor = Vue.extend(DialogComponent);
+      const instance = new DialogConstructor().$mount();
+      const containerDom = document.createElement('div');
+      containerDom.setAttribute('id', domId);
+      containerDom.appendChild(instance.$el);
+      document.body.appendChild(containerDom);
+
       if (options && typeof options === 'object') {
         Object.keys(options).forEach((key) => {
           instance[key] = options[key];
