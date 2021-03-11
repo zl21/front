fa5053b1558822bb1accc429fc8a336632fd9b7a
843748862@qq.com <843748862@qq.com>
2021-03-11 14:21:36 +0800

    修改版本号v1.4.40

--- index.publish.js
+++ index.publish.js
@@ -36,7 +36,7 @@ const validateConfig = config => ({
   message: 'xxx'
 });
 const packageMessage = {
-  version: '1.4.39',
+  version: '1.4.40',
   packageTime: new Date().setNewFormt('yyyy-MM-dd hh:mm:ss'), 
   user: 'local',   
 };
