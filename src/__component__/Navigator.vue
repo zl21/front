eea49aeae0630c6c58c6a5fbbe6e49586d43aea1
liu.li <li.liu@burgeon.cn>
2021-05-06 10:45:25 +0800

    异步消息兼容1.3

--- src/__component__/Navigator.vue
+++ src/__component__/Navigator.vue
@@ -126,6 +126,15 @@
       :closable="false"
     >
       <messagePanel
+        v-if="Version==='1.4'"
+        :panel="messagePanel"
+        @markRead="markReadNote"
+        @ignoreMsg="ignoreMsg"
+        @jumpTask="jumpTask"
+        @nextPage="nextPage"
+      />
+      <message-panel-older
+        v-if="Version==='1.3'"
         :panel="messagePanel"
         @markRead="markReadNote"
         @ignoreMsg="ignoreMsg"
@@ -168,6 +177,8 @@
   import NavigatorPrimaryMenu from './NavigatorPrimaryMenu';
   import SetPanel from './SetPanel';
   import messagePanel from './messagePanel';
+  import messagePanelOlder from './messagePanelOlder'; // 1.3
+
   import ComAutoComplete from './ComAutoComplete';
   import Dialog from './Dialog.vue';
   import { routeTo } from '../__config__/event.config';
@@ -187,6 +198,7 @@
       Dialog,
       NavigatorSubMenu,
       messagePanel,
+      messagePanelOlder,
       ComAutoComplete
     },
     
@@ -218,6 +230,7 @@
         }, // 弹框配置信息
         dialogComponentName: null,
         togglePrimaryMenuData: [],
+        Version: Version(),
         messageTimer: null
       };
     },
@@ -334,14 +347,23 @@
           self.messagePanel.start = start;
           self.messagePanel.list = [];
         }
+        let fixedcolumns = {};
+        if (Version() === '1.3') {
+          fixedcolumns = {
+            OPERATOR_ID: [this.userInfo.id],
+            READSTATE: ['=0'],
+            TASKSTATE: ['=2', '=3']
+          };
+        } else {
+          fixedcolumns = {
+            OPERATOR_ID: [this.userInfo.id],
+            READ_STATE: ['=0'],
+          };
+        }
         const searchdata = {
           table: Version() === '1.3' ? 'CP_C_TASK' : 'U_NOTE',
           column_include_uicontroller: true,
-          fixedcolumns: {
-            OPERATOR_ID: [this.userInfo.id],
-            READ_STATE: ['=0'],
-            // TASKSTATE: ['=2', '=3']
-          },
+          fixedcolumns,
           multiple: [],
           startindex: self.messagePanel.start,
           range: 20,
@@ -357,7 +379,7 @@
           
           if (result.code === 0) {
             self.messagePanel.list = self.messagePanel.list.concat(result.datas.row);
-            console.log(99, self.messagePanel.list);
+            console.log(result, self.messagePanel.list);
 
             self.messagePanel.start = result.datas.start + result.datas.rowCount;
             self.messagePanel.total = result.datas.totalRowCount;
