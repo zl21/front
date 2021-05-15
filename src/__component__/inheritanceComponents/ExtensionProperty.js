b94fd4b7191a62fbb1f1e59fce4d227a86bbb184
chenzihao <278199196@qq.com>
2021-05-16 00:15:58 +0800

    fix: 修复扩展属性bug

--- src/__component__/inheritanceComponents/ExtensionProperty.js
+++ src/__component__/inheritanceComponents/ExtensionProperty.js
@@ -2,7 +2,7 @@
  * 扩展属性组件的自定义逻辑处理
  */
 import { ExtensionProperty } from '@syman/ark-ui-bcl'
-// import ExtensionProperty from 'arkui_BCL/ExtensionProperty';
+// import ExtensionProperty from 'arkui_BCL/ExtensionProperty'
 import network from '../../__utils__/network'
 import { SetDisable } from './setProps'
 class CustomExtensionProperty {
@@ -19,7 +19,6 @@ class CustomExtensionProperty {
       props: this.props,
     }
   }
-
   // 合并props
   mergeProps() {
     const props = {
@@ -27,7 +26,10 @@ class CustomExtensionProperty {
       disabled: new SetDisable(this.item).init(),
       defaultData: this.item.valuedata || {},
       network,
-      ctrlOptions: this.item.ctrlOptions || {rows: 8}
+      ctrlOptions: this.item.ctrlOptions || { rows: 8 },
+      webconf: {
+        ...this.item,
+      },
     }
 
     this.props = props
