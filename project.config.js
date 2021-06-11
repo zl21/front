44f4b583405efcab18c6bf765925cc37ee830f5c
chenzihao <278199196@qq.com>
2021-06-11 09:26:04 +0800

    update: 打开网关

--- project.config.js
+++ project.config.js
@@ -47,7 +47,7 @@ module.exports = {
   
   Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
-  enableGateWay: false, // 网关是否打开,
+  enableGateWay: true, // 网关是否打开,
   enableHistoryAndFavorite: true, // 是否开启收藏与最近使用接口请求
   enableHistoryAndFavoriteUI: true, // 是否显示收藏与最近使用UI
   ignoreGateWayPattern: [/\/jflow\/*/g, /\/api\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配
