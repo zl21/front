17fe1075ec3e199fc0b40b860abf53f50a0394f7
liu.li <li.liu@burgeon.cn>
2021-06-23 10:20:37 +0800

    子表功能测试

---
+++ src/__component__/FormComponents/childrenForm/form.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    23232323
+     <panelForm
+       v-bind="$attrs"
+      :defaultData="formList"
+    ></panelForm>
+</div>
+</template>
+<script>
+export default {
+      name:'childrenForm',
+      data(){
+          return {
+                formList: {
+                    defaultData:{
+                        addcolums:[],
+                        objviewcol:4
+                    }
+                },  //控制面板的展开数据
+            }
+
+      },
+      props:{
+          defaultData: {  //表单数据
+            type: [Array, Object]
+          },
+      },
+      methods:{
+          setFormList(){
+              this.formList.defaultData.addcolums = this.defaultData.inpubobj.concat([]);
+          }
+
+      },
+      watch:{
+        defaultData: {
+            handler () {  //处理展开面板的默认值
+                this.setFormList();
+
+            },
+            deep: true
+        },
+
+      },
+      mounted(){
+          this.setFormList();
+      }
+
+};
+
+</script>
+
