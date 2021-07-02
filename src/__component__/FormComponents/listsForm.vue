636e03bcef859b3546cff9d7894f2befeb697333
liu.li <li.liu@burgeon.cn>
2021-07-02 11:22:49 +0800

    兼容1.3 数据抛出

--- src/__component__/FormComponents/listsForm.vue
+++ src/__component__/FormComponents/listsForm.vue
@@ -98,10 +98,10 @@
         let arrjson = this.dealData(item, val);
         if(item.fkobj && item.fkobj.searchmodel){
                if(Version()==='1.3'){
-                 if(!Array.isArray(json[item.colname])){
-                  if(json[item.colname]){
-                      let id = json[item.colname].split(',');
-                      json[item.colname] = id;
+                 if(!Array.isArray(arrjson[item.colname])){
+                  if(arrjson[item.colname]){
+                      let id = arrjson[item.colname].split(',');
+                      arrjson[item.colname] = id;
                   }
                  }
                }
