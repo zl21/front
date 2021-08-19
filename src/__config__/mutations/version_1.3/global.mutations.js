e481346d4d9ff7a621b32350eb5b6a9878ccdcfe
liu.li <li.liu@burgeon.cn>
2021-08-19 18:05:13 +0800

    修改返回按钮显示逻辑

--- src/__config__/mutations/version_1.3/global.mutations.js
+++ src/__config__/mutations/version_1.3/global.mutations.js
@@ -877,16 +877,16 @@ export default {
     // isSetQuery:可设置目标界面为标准列表界面的表单默认值
     // queryData：设置目标界面表单默认值数据,
     // NToUpperCase:url不转大写,
-    if(original){
-      // 外链跳转字段 校验是否刷新界面
-      let totableName = tableName ||customizedModuleName || linkName;
-      let toMainId = tableId ||customizedModuleId || linkId;
-      let toId = id;
-      let checked = verifyRouter(totableName,toMainId,toId,state.openedMenuLists,arguments,this,state);
-      if(checked){
-        return true;
-      }
-    }
+    // if(original){
+    //   // 外链跳转字段 校验是否刷新界面
+    //   let totableName = tableName ||customizedModuleName || linkName;
+    //   let toMainId = tableId ||customizedModuleId || linkId;
+    //   let toId = id;
+    //   let checked = verifyRouter(totableName,toMainId,toId,state.openedMenuLists,arguments,this,state);
+    //   if(checked){
+    //     return true;
+    //   }
+    // }
     if ((type === 'S' || type === 'STANDARD_TABLE_LIST_PREFIX') && isSetQuery && queryData) {
       if (queryData.values && queryData.values.length > 0) {
         let flag = true;
