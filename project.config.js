086b6bd35bf418bf916f5627949df640bf079f8e
liu.li <li.liu@burgeon.cn>
2021-08-20 23:13:26 +0800

    关闭默认过滤功能

--- project.config.js
+++ project.config.js
@@ -191,7 +191,7 @@ module.exports = {
 
   autoGatewayUrl:false,  // 是否请求网关
   computeForSubtable: false,
-  routerFilter:true, // 跳转是否过滤
+  routerFilter:false, // 跳转是否过滤
   enableLoginPro: false, // 是否开启普通登录模式 false普通 true手机验证码
   //filterURL: [/\/p\/c\/code\/login*/g, /\/p\/c\/message\/login*/g],
   //listFormWebconf:['filtercolval'] // 列表是否支持webcof 字段
