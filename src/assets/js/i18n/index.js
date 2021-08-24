337a77b385ef8567dd1205fead3579b499e770aa
chenzihao <278199196@qq.com>
2021-08-24 21:52:58 +0800

    fix: 1.国际化接口网关问题   2.没开启ark-ui国际化开关

--- src/assets/js/i18n/index.js
+++ src/assets/js/i18n/index.js
@@ -1,14 +1,17 @@
 // 国际化
 import VueI18n from 'vue-i18n'
 import { locale } from '@syman/ark-ui-bcl/src/utils/i18n'
+import { locale as arkLocale } from '@syman/ark-ui';
+import en from '@syman/ark-ui/dist/locale/en-US';
+import zh from '@syman/ark-ui/dist/locale/zh-CN';
 import network, { urlSearchParams } from '../../../__utils__/network';
 
 Vue.use(VueI18n) ;
 
-const lang = localStorage.getItem('r3-lang')
+const lang = localStorage.getItem('r3-lang') || 'zh'
 
 const i18n = new VueI18n({
-    locale: lang || 'en',    // 语言标识, 通过切换locale的值来实现语言切换,this.$i18n.locale 
+    locale: lang,    // 语言标识, 通过切换locale的值来实现语言切换,this.$i18n.locale 
     messages: {
       'zh': require('./zh').default,   // 中文语言包
       'en': require('./en').default    // 英文语言包
@@ -16,11 +19,16 @@ const i18n = new VueI18n({
     // silentTranslationWarn: true // 去掉警告
 })
 
+Vue.use(Ark, { locale: lang === 'en' ? en : zh })  // 设置ark-ui语言
+
 window.R3I18n = function (language) {
-  network.post('/p/cs/langswitcher', urlSearchParams({language: lang})).then(res => {
+  const serviceId = window.localStorage.getItem('serviceId')
+  const url = serviceId ? `/${serviceId}/p/cs/langswitcher`: '/p/cs/langswitcher'
+  network.post(url, urlSearchParams({language: lang}),{noServiceId: true}).then(res => {
     if (res.data.code === 0) {
       i18n.locale = language
       locale(language) // 设置业务组件库语言
+      Vue.use(Ark, { locale: language === 'en' ? en : zh }) // 设置ark-ui语言
       localStorage.setItem('r3-lang', language)
     }
   })
