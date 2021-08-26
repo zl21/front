376fd90673044f2c8804aa99aacb569ce583f46f
xc9010 <xuchen199010@gmail.com>
2021-08-26 13:59:56 +0800

    fix-network-返回非json不提示的问题

--- src/__utils__/utils.js
+++ src/__utils__/utils.js
@@ -3,3 +3,18 @@ import { filterURL } from '../constants/global';
 const urls = filterURL ? [/\/p\/c\/code\/login*/g, /\/p\/c\/message\/login*/g].concat(filterURL) : [/\/p\/c\/code\/login*/g, /\/p\/c\/message\/login*/g];
 
 export const filterUrl = (url) => urls.some(v => url.match(v));
+
+
+export const isJSON = (str) => {
+  if (typeof str == 'string') {
+    try {
+      var obj = JSON.parse(str);
+      if (obj && typeof obj === 'object' && obj !== null) {
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+  return false;
+};
