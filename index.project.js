2644c67bececde585b5e5ed98351071f315c0be7
liu.li <li.liu@burgeon.cn>
2021-08-30 20:46:44 +0800

    添加水平布局

--- index.project.js
+++ index.project.js
@@ -16,7 +16,7 @@ const {
 Vue.prototype.$network = network;
 Vue.prototype.$urlSearchParams = urlSearchParams;
 R3.launchApplication({
-  layoutDirection: true,
+  layoutDirection: false,
   externalTreeDatas: externalTreeDatasConfig,
   // appLayout: require('./src/config/appLayout.js').default,
   ignoreGateWayPattern: [/\/jflow\/*/g, /\/api\/*/g], // 框架默认禁用的网关逻辑的正则模式匹配
