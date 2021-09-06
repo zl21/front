08ce6a04367b1da21c7f6f44033e7265cf5dbc06
liu.li <li.liu@burgeon.cn>
2021-09-06 17:57:23 +0800

    修改发版配置

--- project.config.js
+++ project.config.js
@@ -161,8 +161,8 @@ module.exports = {
   agGridOptions: { // ag表格的配置
     // rowHeight: 100
   },
-  messageSwitch: true, // 消息开关
-  useAgGrid: true, // 明细界面的表格全局用ag表格进行渲染
+  messageSwitch: false, // 消息开关
+  useAgGrid: false, // 明细界面的表格全局用ag表格进行渲染
   // (data) => {
   //   if (data.router.params) {
   //     if (data.router.params.tableName && data.config().configPage[data.router.params.tableName].filter(u => u === data.url).length > 0) { // 筛选出配置界面不需要根据框架code报错提示框的接口
