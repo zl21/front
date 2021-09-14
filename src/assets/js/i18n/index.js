750b997851cf4d5a1721067c0e58880136421d30
liu.li <li.liu@burgeon.cn>
2021-09-14 10:13:45 +0800

    修改发版版本号

--- src/assets/js/i18n/index.js
+++ src/assets/js/i18n/index.js
@@ -18,7 +18,6 @@ window.R3_lang = {
   en: require('./en').default, // 英文语言包
 }
 
-const lang = localStorage.getItem('r3-lang') || 'zh' // 获取本地缓存语言。用于刷新界面后保持语言不变
 const i18n = new VueI18n({
   locale: lang, // 语言标识, 通过切换locale的值来实现语言切换,this.$i18n.locale
   messages: {
