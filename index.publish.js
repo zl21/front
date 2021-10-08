eb92f2cfa471107acb50e3494f7a1eafad909a08
chenzihao <278199196@qq.com>
2021-10-08 15:01:21 +0800

    feat: 更改导入模块

--- index.publish.js
+++ index.publish.js
@@ -37,11 +37,11 @@ import externalComponents from './src/__config__/externalComponents'
 // import SelectTree from './src/__component__/Tree/SelectTree.vue';
 // import DocFile from './src/__component__/docfile/DocFileComponent.vue';
 // import Login from './src/__component__/Login/LoginCore'; // 登录核心功能
-import ErCode from './src/__component__/Login/components/ErCode'; // 验证码
-import Collect from './src/__component__/nav/collect'; // 收藏
-import Lately from './src/__component__/nav/lately'; // 最近访问
-import ChangeLang from './src/__component__/Login/components/ChangeLang'; // 语言切换
-import HistoryAndFavorite from './src/__component__/HistoryAndFavorite'; // 水平排版下的收藏和最近访问
+// import ErCode from './src/__component__/Login/components/ErCode'; // 验证码
+// import Collect from './src/__component__/nav/collect'; // 收藏
+// import Lately from './src/__component__/nav/lately'; // 最近访问
+// import ChangeLang from './src/__component__/Login/components/ChangeLang'; // 语言切换
+// import HistoryAndFavorite from './src/__component__/HistoryAndFavorite'; // 水平排版下的收藏和最近访问
 import { menuClick } from './src/__config__/event.config';
 import { connector } from './src/constants/global';
 import './src/constants/dateApi';
@@ -247,10 +247,10 @@ export default {
   },
   components: {
    ...externalComponents,
-    ErCode,
-    Collect,
-    Lately,
-    ChangeLang,
-    HistoryAndFavorite,
+    // ErCode,
+    // Collect,
+    // Lately,
+    // ChangeLang,
+    // HistoryAndFavorite,
   }
 };
