ff1e0dd732093d2376e8056a31a95fe6abf324c5
liu.li <li.liu@burgeon.cn>
2021-11-05 13:37:05 +0800

    添加测试分享

---
+++ share/entry.constant.js
@@ -0,0 +1,218 @@
+import md5 from 'md5'
+import packJson from '../package.json'
+import { getGuid } from '../src/__utils__/random'
+import router from '../src/__config__/router.config';
+import store from '../src/__config__/store.config'
+import { enableInitializationRequest, specifiedGlobalGateWay, backDashboardRoute, HAS_BEEN_DESTROYED_MODULE } from '../src/constants/global'
+import i18n from '../src/assets/js/i18n'
+import network from '../src/__utils__/network';
+import { DispatchEvent } from '../src/__utils__/dispatchEvent';
+import { getLocalObject } from '../src/__utils__/localStorage';
+import { removeSessionObject, getSessionObject } from '../src/__utils__/sessionStorage';
+import getObjdisType from '../src/__utils__/getObjdisType';
+// import App from '../../App.vue';
+import App from '../src/__component__/ContentDisplayArea.vue';
+
+
+window.vm = {
+  
+}
+// 挂载router和store
+window.vm.$router = router;
+window.vm.$store = store;
+
+const packageMessage = {
+  version: packJson.version,
+  packageTime: new Date(),
+  user: 'local',
+}
+
+function hookAJAX() {
+  XMLHttpRequest.prototype.nativeOpen = XMLHttpRequest.prototype.open
+  var customizeOpen = function(method, url, async, user, password) {
+    this.nativeOpen(method, url, async, user, password)
+    let number = Math.floor(Math.random() * 10000)
+    let sessionCookie = window.localStorage.getItem('sessionCookie')
+    this.setRequestHeader('SSSSS-A', new Date().getTime())
+    if (sessionCookie === 'undefined') {
+      this.setRequestHeader(
+        'SSSSS-B',
+        md5('qwertburgeon' + new Date().getTime() + number)
+      )
+    } else {
+      this.setRequestHeader(
+        'SSSSS-B',
+        md5('qwertburgeon' + new Date().getTime() + number + sessionCookie)
+      )
+    }
+    this.setRequestHeader('SSSSS-C', number)
+  }
+
+  XMLHttpRequest.prototype.open = customizeOpen
+}
+
+const createDOM = () => {
+  const div = document.createElement('div')
+  div.setAttribute('id', getGuid())
+  document.body.appendChild(div)
+  return div
+}
+
+const backTouristRoute = () => {
+  // window.sessionStorage.setItem('loginStatus', false);// 清除登陆标记
+  // router.push({ path: getTouristRoute() });
+  store.dispatch('global/signout')
+}
+
+const setMessage = (data) => {
+  window.vm.$Modal.fcError({
+    title: i18n.t('feedback.alert'),
+    content: data.content,
+    cancelType: true,
+    titleAlign: 'left',
+    mask: true,
+    draggable: true,
+    closable: false,
+    onCancel: () => {
+      backTouristRoute()
+    },
+    onOk: () => {
+      backTouristRoute()
+    },
+  })
+}
+
+const getCategory = () => {
+  if (enableInitializationRequest()) {
+    network
+      .post('/p/cs/getSubSystems')
+      .then((res) => {
+        if (res.data.code === '-1') {
+          backTouristRoute()
+        } else if (res.data.data.length > 0) {
+          store.commit('global/updateMenuLists', res.data.data)
+          const serviceIdMaps = res.data.data
+            .map((d) => d.children)
+            .reduce((a, c) => a.concat(c), [])
+            .map((d) => d.children)
+            .reduce((a, c) => a.concat(c), [])
+            .filter(
+              (d) =>
+                d.type === 'table' || d.type === 'action' || d.type === 'tree'
+            )
+            .reduce((a, c) => {
+              a[c.value.toUpperCase()] = c.serviceId
+              return a
+            }, {})
+          window.localStorage.setItem(
+            'serviceIdMap',
+            JSON.stringify(serviceIdMaps)
+          )
+          DispatchEvent('gatewayReady')
+        } else if (getLocalObject('loginStatus') === true) {
+          setMessage({ content: i18n.t('messages.NoMenuPermission') })
+        }
+      })
+      .catch(() => {
+        if (getSessionObject('loginStatus') === true) {
+          setMessage({ content: i18n.t('messages.NoMenuPermission') })
+        }
+      })
+  }
+}
+
+const init = () => {
+  removeSessionObject(HAS_BEEN_DESTROYED_MODULE);
+  const rootDom = createDOM();
+  window.vm = new Vue({
+    router,
+    store,
+    i18n,
+    render: createElement => createElement(App)
+  }).$mount(rootDom);
+  
+  window.R3message = (data) => {
+    window.vm.$Modal.fcError({
+      mask: data.mask,
+      titleAlign: 'center',
+      title: data.title,
+      // content: formatJsonEmg
+      render: h => h('div', {
+        style: {
+          padding: '10px 20px 0',
+          display: 'flex',
+          // alignItems: 'center',
+          lineHeight: '16px'
+        }
+      }, [
+        
+        h('i', {
+          props: {
+          },
+          style: {
+            marginRight: '5px',
+            display: 'inline-block',
+            'font-size': '28px',
+            'margin-right': ' 10px',
+            'line-height': ' 1',
+            padding: ' 10px 0',
+            color: 'red'
+          },
+          class: 'iconfont iconbj_error fcError '
+        }),
+        h('div', {
+          attrs: {
+          },
+          domProps: {
+          },
+          style: `width: 80%;
+              margin: 1px;
+              margin-bottom: -8px;
+              box-sizing: border-box;
+              padding: 5px;
+              resize: none;
+              max-height: 100px;
+              max-width: 300px;
+              overflow: auto;
+              `
+        }, data.content)
+      ])
+    });
+  };
+
+  window.getObjdisType = getObjdisType;
+  if (backDashboardRoute().filter(path => path === router.currentRoute.fullPath).length > 0) {
+    router.push('/');
+    setTimeout(() => {
+      store.commit('global/updataOpenedMenuLists', []);
+    }, 500);
+  }
+  DispatchEvent('initReady');
+};
+
+const getGateWayServiceId = () => {
+  if (enableInitializationRequest()) {
+    if (specifiedGlobalGateWay()) {
+      window.localStorage.setItem('serviceId', specifiedGlobalGateWay());
+      getCategory();
+      setTimeout(() => {
+        init();
+      }, 0);
+    } else {
+      network.get('/p/c/get_service_id').then((res) => {
+        window.localStorage.setItem('serviceId', res.data.data.serviceId);
+        getCategory();
+        setTimeout(() => {
+          DispatchEvent('serviceIdReady', {
+            detail: {
+              serviceId: res.data.data.serviceId
+            }
+          });
+          init();
+        }, 0);
+      });
+    }
+  }
+};
+
+export { packageMessage, hookAJAX, createDOM, getCategory, getGateWayServiceId, init }
--- share/index.share.js
+++ share/index.share.js
@@ -1,51 +1,20 @@
 import store from '../src/__config__/store.config';
-import App from '../src/App.vue';
-import i18n from '../src/assets/js/i18n';
 import network from '../src/__utils__/network';
 import {
-  backDashboardRoute, enableGateWay, enableInitializationRequest, HAS_BEEN_DESTROYED_MODULE, specifiedGlobalGateWay
+  enableGateWay, enableInitializationRequest, HAS_BEEN_DESTROYED_MODULE, specifiedGlobalGateWay
 } from '../src/constants/global';
-import { removeSessionObject, getSessionObject } from '../src/__utils__/sessionStorage';
-import { getLocalObject } from '../src/__utils__/localStorage';
 
 import customizedModalConfig from '../src/__config__/customizeDialog.config';
 import Loading from '../src/__utils__/loading';
-import getObjdisType from '../src/__utils__/getObjdisType';
 import projectConfig from '../projectConfig/project.config.js';
 import '../src/assets/js/entry.common'
-import { packageMessage, hookAJAX, getCategory, getGateWayServiceId, init } from '../src/assets/js/entry.constant'
+import { packageMessage, hookAJAX, getCategory, getGateWayServiceId, init } from '../share/entry.constant.js'
 
 // css import
 // 组件css汇总
 import '../src/index.less';
 import '../src/assets/theme/custom.less';
 
-// import jflowPlugin,  { components } from '@syman/jflow-plugin';
-// import '@syman/jflow-plugin/dist/jflowPlugin.min.css'
-
-// Vue.use(jflowPlugin, {
-//   changePattern: true, // 控制待办列表转派的选择模式 true为单选,false为多选
-//   gateway: null  //jflow网关配置
-// });
-
-
-// import jflowPlugin from './jflowPlugin/js/index';
-// import '../src/__utils__/encryptingParameter';
-
-
-
-
-// import jflowPlugin from './jflowPlugin/js/index';
-// import '../src/__utils__/encryptingParameter';
-
-
-
-
-// Vue.use(jflowPlugin, {
-//   changePattern: true, // 控制待办列表转派的选择模式 true为单选,false为多选
-//   gateway: '',
-//   roleSwitch: false
-// });
 
 projectConfig.packageMessage = packageMessage;
 window.ProjectConfig = projectConfig;
@@ -65,144 +34,7 @@ registerCustomizedModal();
 
 Vue.use(Loading);
 
-// const backTouristRoute = () => {
-//   // window.sessionStorage.setItem('loginStatus', false);// 清除登陆标记
-//   // router.push({ path: getTouristRoute() });
-//   store.dispatch('global/signout');
-// };
 
-// const setMessage = (data) => {
-//   window.vm.$Modal.fcError({
-//     title: i18n.t('feedback.alert'),
-//     content: data.content,
-//     cancelType: true,
-//     titleAlign: 'left',
-//     mask: true,
-//     draggable: true,
-//     closable: false,
-//     onCancel: () => {
-//       backTouristRoute();
-//     },
-//     onOk: () => {
-//       backTouristRoute();
-//     },
-//   });
-// };
-
-// const init = () => {
-  // removeSessionObject(HAS_BEEN_DESTROYED_MODULE);
-
-  // // 初始化配置的图片
-  // if (projectConfig && projectConfig.image) {
-  //   store.commit('global/updateImage', projectConfig.image);
-  // }
-
-  // const rootDom = createDOM();
-  // window.vm = new Vue({
-  //   router,
-  //   store,
-  //   i18n,
-  //   render: createElement => createElement(App)
-  // }).$mount(rootDom);
-  // if (backDashboardRoute().filter(path => path === router.currentRoute.fullPath).length > 0) {
-  //   router.push('/');
-  //   setTimeout(() => {
-  //     store.commit('global/updataOpenedMenuLists', []);
-  //   }, 500);
-  // }
-  // window.R3message = (data) => {
-  //   window.vm.$Modal.fcError({
-  //     mask: data.mask,
-  //     titleAlign: 'center',
-  //     title: data.title,
-  //     // content: formatJsonEmg
-  //     render: h => h('div', {
-  //       style: {
-  //         padding: '10px 20px 0',
-  //         display: 'flex',
-  //         // alignItems: 'center',
-  //         lineHeight: '16px'
-  //       }
-  //     }, [
-
-  //       h('i', {
-  //         props: {
-  //         },
-  //         style: {
-  //           marginRight: '5px',
-  //           display: 'inline-block',
-  //           'font-size': '28px',
-  //           'margin-right': ' 10px',
-  //           'line-height': ' 1',
-  //           padding: ' 10px 0',
-  //           color: 'red'
-  //         },
-  //         class: 'iconfont iconbj_error fcError '
-  //       }),
-  //       h('div', {
-  //         attrs: {
-  //         },
-  //         domProps: {
-  //         },
-  //         style: `width: 80%;
-  //             margin: 1px;
-  //             margin-bottom: -8px;
-  //             box-sizing: border-box;
-  //             padding: 5px;
-  //             resize: none;
-  //             max-height: 100px;
-  //             max-width: 300px;
-  //             overflow: auto;
-  //             `
-  //       }, data.content)
-  //     ])
-  //   });
-  // };
-
-
-  // window.getObjdisType = getObjdisType;
-// };
-
-// const getCategory = () => {
-//   if (enableInitializationRequest()) {
-//     network.post('/p/cs/getSubSystems').then((res) => {
-//       if (res.data.code === '-1') {
-//         backTouristRoute();
-//         // window.sessionStorage.setItem('loginStatus', false);// 清除登陆标记
-//         // router.push({ path: getTouristRoute() });
-//       } else if (res.data.data.length > 0) {
-//         store.commit('global/updateMenuLists', res.data.data);
-//         const serviceIdMaps = res.data.data.map(d => d.children)
-//           .reduce((a, c) => a.concat(c))
-//           .map(d => d.children)
-//           .reduce((a, c) => a.concat(c))
-//           .filter(d => d.type === 'table' || d.type === 'action' || d.type === 'tree')
-//           .reduce((a, c) => {
-//             let menuType = '';
-//             if (c.url) {
-//               menuType = c.url.substring(c.url.lastIndexOf('/') + 1, c.url.length);
-//             }
-//             if (menuType === 'New') {
-//               const i = c.url.substring(c.url.indexOf('/') + 1, c.url.lastIndexOf('/'));
-//               const n = i.substring(i.indexOf('/') + 1, i.lastIndexOf('/'));
-//               const name = n.substring(n.lastIndexOf('/') + 1, n.length);
-//               a[name.toUpperCase()] = c.serviceId;
-//             } else {
-//               a[c.value.toUpperCase()] = c.serviceId;
-//             }
-//             return a;
-//           }, {});
-//           //
-//         const getServiceIdMap = JSON.parse(window.localStorage.getItem('serviceIdMap'));
-//         const serviceIdMapRes = Object.assign({}, getServiceIdMap, serviceIdMaps);
-//         window.localStorage.setItem('serviceIdMap', JSON.stringify(serviceIdMapRes));
-//       } else if (getLocalObject('loginStatus') === true) {
-//         // getSessionObject('loginStatus') === true
-//         setMessage({ content: i18n.t('messages.NoMenuPermission') });
-//       }
-//     });
-//   }
-// };
 
 const getSubSystems = () => {
   if (enableInitializationRequest()) {
@@ -215,27 +47,6 @@ const getSubSystems = () => {
 };
 
 
-// const getGateWayServiceId = () => {
-//   if (enableInitializationRequest()) {
-//     if (specifiedGlobalGateWay()) {
-//       window.localStorage.setItem('serviceId', specifiedGlobalGateWay());
-//       getCategory();
-//       setTimeout(() => {
-//         init();
-//       }, 0);
-//     } else {
-//       network.get('/p/c/get_service_id').then((res) => {
-//         if (res.data && res.data.data && res.data.data.serviceId) {
-//           window.localStorage.setItem('serviceId', res.data.data.serviceId);
-//         }
-//         getCategory();
-//         setTimeout(() => {
-//           init();
-//         }, 0);
-//       });
-//     }
-//   }
-// };
 
 
 if (enableGateWay()) {
