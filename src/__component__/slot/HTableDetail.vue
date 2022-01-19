cc5dc80b652fe315a59636b017077bd84f44b509
liu.li <li.liu@burgeon.cn>
2022-01-19 10:36:40 +0800

    提交添加模板问题

---
+++ src/__component__/slot/HTableDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="StandardTableListRootDiv">
+    <slot name="list-buttons"></slot>
+    <slot name="list-form"></slot>
+    <slot name="list-defind"></slot>    
+    <slot name="list-tabBar"></slot>
+    <slot name="list-table"></slot>
+  </div>
+</template>
+<script>
+
+ export default {
+  name: '',
+  inheritAttrs: true,
+
+}
+</script>
