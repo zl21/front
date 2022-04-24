b5920ab1ffc5a1788391e3cfa29c221d0159dc6d
liu.li <li.liu@burgeon.cn>
2022-04-24 14:56:06 +0800

    添加是否存在拦截

--- src/__component__/Navigator.vue
+++ src/__component__/Navigator.vue
@@ -232,8 +232,11 @@
           if (this.$el) {
             this.$el.parentElement.hidden = true;
             this.$el.parentElement.parentElement.hidden = true;
-            this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.firstElementChild.style.padding = '0px';
-            this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.style.margin = '0px';
+            if(this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild){
+               this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.firstElementChild.style.padding = '0px';
+              this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.style.margin = '0px';
+            }
+           
           }
         }
       },
@@ -352,8 +355,11 @@
         if (this.$el) {
           this.$el.parentElement.hidden = true;
           this.$el.parentElement.parentElement.hidden = true;
-          this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.firstElementChild.style.padding = '0px';
-          this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.style.margin = '0px';
+          if(this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild){
+            this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.firstElementChild.firstElementChild.style.padding = '0px';
+            this.$el.parentElement.nextElementSibling.firstElementChild.lastElementChild.style.margin = '0px';
+          }
+          
         }
       }
 
