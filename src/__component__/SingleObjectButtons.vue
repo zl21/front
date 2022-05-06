21b1e3af01a961039d74f2297bd2b2ab36956444
liu.li <li.liu@burgeon.cn>
2022-05-06 11:39:27 +0800

    修改导航样式

--- src/__component__/SingleObjectButtons.vue
+++ src/__component__/SingleObjectButtons.vue
@@ -4101,13 +4101,13 @@ import RouterPush from '../__utils__/routerback';
           }
           return false;
         }
-        let checked = new RouterPush(this).exists(currentRoute);
-        // 判断来源是否存在;
-        if(checked){
-            this.dataArray.back = false;
-        }else{
-           this.dataArray.back = true;
-        }
+        // let checked = new RouterPush(this).exists(currentRoute);
+        // // 判断来源是否存在;
+        // if(checked){
+        //     this.dataArray.back = false;
+        // }else{
+        //    this.dataArray.back = true;
+        // }
         let { ResetrouterBackLogic } = window.ProjectConfig;
         if( ResetrouterBackLogic ){
           return false;
