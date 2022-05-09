966e45c96efc6f474e5621ed40cc51f0c0294aa8
liu.li <li.liu@burgeon.cn>
2022-05-09 15:00:03 +0800

    添加换肤插件引用

--- projectConfig/project.config.1.4.js
+++ projectConfig/project.config.1.4.js
@@ -4,8 +4,8 @@ const project = {
   projectsTitle: 'Burgeon R3 Erp Frame', // 项目title
   projectIconPath: '', // 项目icon,
   // target: 'http://47.99.229.124:1024', // 框架研发网关关闭环境
- // target: 'http://r3-8098.dev.syman.cn/', // 框架研发网关开启环境
-  target:'http://retail-damai-qa.oneretail.cn/',
+ target: 'http://r3-8098.dev.syman.cn/', // 框架研发网关开启环境
+  // target:'http://retail-damai-qa.oneretail.cn/',
   // target: 'http://r3-java.dev.burgeononline.com/', // 框架后端测试分支
   // target: 'http://retail-service-center.dev.burgeononline.com/',
   // target: 'http://syman-jflow-standard.dev.burgeononline.com/', // 新版本jflow测试环境
@@ -25,7 +25,7 @@ const project = {
   // target:'http://139.196.18.137/',  // 云促销
   Version: '1.4', // 版本号
   interlocks: true, // 是否打开三级联动装置
-  enableGateWay: false, // 网关是否打开,
+  enableGateWay: true, // 网关是否打开,
   enableLoginPro:false,
   ...projectconfig,
 };
